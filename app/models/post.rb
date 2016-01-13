027589870054db2bacb3d194fc3fd9ae7227672d
onyoo <roberto9m9@gmail.com>
2016-01-13 14:19:05 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,13 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length:  {maximum: 250}
   validates :category, inclusion: { in: ["Fiction"]}
-  validates :title, format: { with: /(Won't Believe)/}
+  validate :click_bait
+
+
+  def click_bait
+    if !title.nil? && (!title.include? ("You Won't" || "Secret" || "Top" || "Guess"))
+      errors.add(:click_bait, "This ain't click bait! You're fired!")
+    end
+  end
+
 end
