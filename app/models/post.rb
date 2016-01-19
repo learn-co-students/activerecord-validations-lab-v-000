b07674d71301eb594383c4e403eef38d56000449
Lukeghenco <lukeghenco@gmail.com>
2016-01-18 20:34:03 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :click_bait
+
+  def click_bait
+    if !title.nil? && !title.include?("You Won't" || "Secret" || "Top" || "Guess")
+      errors.add(:click_bait, "This is click bait")
+    end
+  end
 end
