f719bdd2feb46e7c9c933f976abb1d66a1012202
febbraiod <donedesign1@gmail.com>
2016-02-17 14:03:41 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+  validate :bait?
+
+
+  def bait?
+    if !self.title || !self.title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+      self.errors[:title] << 'NOT BAIT!'
+    end
+  end
+
 end
