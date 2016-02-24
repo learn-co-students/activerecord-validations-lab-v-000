6129a416764866793a697cfd9a58f88cfba6f626
Adam King <adam.king0126@gmail.com>
2016-02-24 11:54:27 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
+require 'pry'
+
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    if record.title.nil?
+      record.errors[:title] << 'Must have a title'
+    else
+     unless ["Won't Believe", "Secret", "Top [number]", "Guess"].any? {|click_bait| record.title.include?(click_bait) } 
+      record.errors[:title] << 'We demand click-bait!'
+     end 
+    end
+  end
+end
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates_inclusion_of :category, :in => ["Fiction",  "Non-Fiction"]
+  validates_with MyValidator
 end
