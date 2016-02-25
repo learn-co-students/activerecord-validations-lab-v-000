8b1e6631f98e829b899d3e578d6d9949ea76ad42
jaeku <ja.ku@rutgers.edu>
2016-02-25 01:52:48 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true #All authors have a name
+  validates :name, uniqueness: true #Authors name must be unique
+  validates :phone_number, length: { is: 10 } #Author phone numbers are exactly ten digits
 end
