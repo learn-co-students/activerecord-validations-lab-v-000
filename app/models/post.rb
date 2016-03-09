174648bfbcaadc6a71b045749c9917990a1f2a43
azheng249 <andy.zheng249@gmail.com>
2016-03-09 17:25:13 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid size"}
+  validate :clickbait?
+
+  def clickbait?
+    clickbait_phrases = [
+      /Won't Believe/i, 
+      /Secret/i ,
+      /Top [\d]+/i,
+      /Guess/i
+    ]
+
+    # Add show error for title if none of the regex clickbait phrases match the title
+    errors.add(:title, "Must be clickbait") if clickbait_phrases.none? {|clickbait| clickbait.match(title) }
+  end
 end
