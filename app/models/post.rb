32bf6f6178cf673fc29b712fa26fff09f4486929
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-16 23:48:39 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, format: { with: /Won't Believe|Secret|Top \d*|Guess/i, message: "Must have a clickbait-y title" }
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
 end
+
