d21081b68423db02d1fb64f31a6bab9ead3dc580
jsgoldb <jgoldb@yahoo.com>
2016-03-16 14:57:58 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :click_bait
+
+  def click_bait
+    if title
+      unless title.include?("Won't Believe") ||
+        title.include?("Secret") ||
+        title.include?("Top/\d+/") ||
+        title.include?("Guess")
+        errors.add(:title, "not click-baity")
+      end
+    end
+  end
+
+
+
 end
