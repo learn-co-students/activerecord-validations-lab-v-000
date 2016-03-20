4570f8935e7a714419938b7f8e67b35b0b692d2c
drywaterr24 <angelolea24@gmail.com>
2016-03-19 23:11:11 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  CLICKBAIT = ["Won't Believe.", "Secret", "Top/\d+/", "Guess"]
+  validates :title, presence: true, format: {with: /Won't Believe.|Secret|Top\d+|Guess/}
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
