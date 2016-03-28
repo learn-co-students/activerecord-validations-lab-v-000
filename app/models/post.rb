f508d5e1047d5f5807bb765fb86bfc0468a0177f
lizdenhup <lizdenhup@gmail.com>
2016-03-27 20:12:22 -0500

    finished validation lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true 
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Non-Fiction Fiction)}
+  validate :is_clickbait?
+
+  CLICKBAIT = [
+    /Won't Believe/i,
+    /Secret/,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+
+  def is_clickbait?
+    if CLICKBAIT.none? {|instance| instance.match title }
+        errors.add(:title, "must be clickbait")
+      end 
+    end 
+
 end
