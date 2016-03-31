2b9a0124f8db42dcc2819c4c36771b647046e21e
fiveinfinity <jlachance1@gmail.com>
2016-03-31 09:32:03 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 50}
+  validates :summary, length: {maximum: 20}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction Biography)}
+  validate :clickbait?
+
+  PHRASES = [/Won't Believe/i, /Secret/i, /Top[0-9]*/i, /Guess/i]
+
+  def clickbait?
+    if PHRASES.none? {|phrase| phrase.match title}
+      errors.add(:title, "Not enough clickbait")
+    end
+  end
 end
