89dcba8ec1155b9b765049777520121cfcbc6756
the-widget <brennenawana108ny@gmail.com>
2016-04-01 20:39:43 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, length: {minimum: 2}
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :click_baity
+
+  def click_baity
+    if title == nil || title.scan(/(Won't Believe|Guess|Secret|Top \d?)/).empty?
+      errors.add(:title, "You need a catchier title!")
+    end
+  end
+
+
+
+
 end
