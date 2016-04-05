06afd48d37252333b6af484953e36b78f4c1f38d
sameera91 <sameera.bachoti@gmail.com>
2016-04-05 19:08:16 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  #validates :title, inclusion: { in: %w(/(Won't Believe)|(Secret)|(Guess)|(Top\s\d)/) }
+  validate :clickbait
+
+  def clickbait
+    if !["Won't Believe","Secret","Guess","Top[/d]"].any? { |phrase| self.title.to_s.include?(phrase) }
+      errors.add(:name, "please include a clickbait-y title")
+    end
+  end
 end
