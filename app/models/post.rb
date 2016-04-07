12b84a2bace7e8835c39ae3edaac78c5f15e9c77
LKWLaLa <lkwlala@gmail.com>
2016-04-07 13:15:22 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbate
+
+  def clickbate
+   title_options = /Won't Believe|Secret|Top[0-9]|Guess/
+   if self.title.scan(title_options).length < 1
+    errors.add(:title, "This title must contain some clickbate!")
+   end if self.title
+  end
 end
