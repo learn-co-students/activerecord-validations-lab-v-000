197ad0db48e3c5171d66e7034c7549847149e2d5
evanscloud <dijkstrascloud@gmail.com>
2016-04-12 22:17:02 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 50 }
+  validates :summary, length: { maximum: 25 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validates :title, format: { with: /(Won't Believe)|(Secret)|(Guess)|(Top\s\d)/}
 end
