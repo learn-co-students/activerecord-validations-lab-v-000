94592a421a502580f1875957d6781716feb22d17
e-baker <eric@ericbaker.me>
2016-04-20 03:52:25 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+
+  CB_VALUES = [
+    /Won't Believe/i, 
+    /Secret/i, 
+    /Top [0-9]/i,
+    /Guess/i
+  ]
+  
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate  :clickbait?
+
+  def clickbait?
+
+    if !CB_VALUES.any? { |value| value.match title }
+      errors.add(:title, "C'mon man. That's not clickbait!")
+    end
+  end
 end
