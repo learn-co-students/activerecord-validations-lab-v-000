50469e7c5e0b841f18a673e2a73add06d90b4b5e
glee38 <glee3388@gmail.com>
2016-04-21 18:35:09 -0400

    add alternate solutions

--- app/models/clickbait.rb
+++ app/models/clickbait.rb
@@ -2,7 +2,7 @@ class ClickBaitValidator < ActiveModel::Validator
   def validate(record)
     if !record.title.nil?
       unless record.title.include?("Won't Believe" || "Secret" || /([Top]\s\d+)/ || "Guess")
-        record.errors.add(:title, "C'mon, you can do better than that.")
+        record.errors.add(:title, "C'mon, you can be more click-baity than that.")
       end
     end
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,29 @@ class Post < ActiveRecord::Base
   
   include ActiveModel::Validations
   validates_with ClickBaitValidator
+
+  # can also do:
+  # validates :title, exclustion: { in: ["Won't Believe", "Secret", /Top [0-9]*/i, "Guess", "True Facts"], message: "your error message"}
+
+  # solution on learn
+
+  # validate :is_clickbait?
+
+    # CLICKBAIT_PATTERNS = [
+    #   /Won't Believe/i,
+    #   /Secret/i,
+    #   /Top [0-9]*/i,
+    #   /Guess/i
+    # ]
+
+    # def is_clickbait?
+    # if title == nil || title.scan(/(Won't Believe|Guess|Secret|Top \d?)/).empty?
+    
+    # or
+    
+    # if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+    
+    #     errors.add(:title, "must be clickbait")
+    #   end
+    # end
 end
