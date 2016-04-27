f859373756ce4870c38f6acb92ccedb0eec1aceb
jenjiyi <jenny.yamada@gmail.com>
2016-04-27 12:27:49 -0700

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
+
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true, format: { with: 
+/Won't Believe|Secret|Top \d|Guess/, message: "not clickbait" }
+  validates :content, length: {minimum: 40}
+  validates :summary, length: {maximum: 100}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+  message: "%{value} is not a valid category" }
 end
