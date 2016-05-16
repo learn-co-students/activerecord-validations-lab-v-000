34dc428d7fdefac26f2615396bced36ef94c64a5
gnfisher <me@gnfisher.com>
2016-05-16 09:53:24 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :clickbaity
+
+  def clickbaity
+    clickbait = [
+      "Won't Believe",
+      "Secret",
+      "Top",
+      "Guess" ]
+    
+    reg = /#{clickbait.map{|w| Regexp.escape(w)}.join('|')}/
+    unless reg === title
+      errors.add(:title, "Not click bait")
+    end
+  end
 end
