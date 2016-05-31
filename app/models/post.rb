a01eff7c7efe65ec035d5616f956190779487742
kriskanya <kriskanya@gmail.com>
2016-05-31 13:59:06 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+class ClickBaitValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.title =~ /won't believe|secret|top \d+|guess/i
+      record.errors[:title] << 'Need a clickbaity link'
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :summary, length: { maximum: 299 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), 
+    message: "%{value} is not a valid category" }
+  validates_with ClickBaitValidator
+
 end
