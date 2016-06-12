5b0b1bbaadcd0b1e8caf56d9483bbe08bf69f8ef
theRingleman <sam.ringleman@gmail.com>
2016-06-12 07:33:40 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :click_bait
+
+
+  def click_bait
+    if !title.nil? && (!title.include?("Won't Believe" || "Secret" || "Top" || "Guess"))
+      errors.add(:click_bait, "Sorry, article not fully ready to annoy.")
+    end
+  end
+
 end
