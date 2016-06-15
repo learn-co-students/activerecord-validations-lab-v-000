6c5c2ce590d182430901663c329e33588abeb928
breiss <brad.p.reiss@gmail.com>
2016-06-15 06:44:48 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
+class Clickbait < ActiveModel::Validator
+  def validate(x)
+        unless x.title.nil?
+            unless x.title.index(/(Won't Believe)|(Guess)|(Secret)|(Top \d+)/)
+                x.errors[:title] << 'needs more clickbait'
+            end
+        end
+  end
+end
+
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates_with Clickbait
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
 end
