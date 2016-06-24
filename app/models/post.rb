c1e0dbce7f3f8dfc854e966f83d6327b741fc4a0
jmheyd <heydjm@comcast.net>
2016-06-23 23:41:00 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+	validate :title_is_clickbaity
+
+	def title_is_clickbaity
+		clickbait_array = [/Won't Believe/i, /Guess/i, /Secret/i, /Top [0-9]*/i]
+		re = Regexp.union(clickbait_array)
+		if !title.nil?
+			match = title.match(re).nil?
+			if match == true
+				errors.add(:title, "Title is not clickbait-y")
+			end
+		end
+	end
+	
 end
