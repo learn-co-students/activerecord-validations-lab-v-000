6d0de87a53bb13dfd7b86f4110f82340c1ec52e7
hangloos <ericloos00@gmail.com>
2016-06-28 17:42:38 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+validates :title, presence: true
+validates :content, length: { minimum: 250 }
+validates :summary, length: { maximum: 250 }
+validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+  def validate
+    if /(Won't Believe)|(Secret)|(Guess)|((Top) *[0-9])/.match(self.title) == nil
+      false
+    else
+      true
+    end
+  end
+
+
 end
