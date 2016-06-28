327d518e44b95d3c46303e0ce938ab1578c5bc58
hangloos <ericloos00@gmail.com>
2016-06-28 18:08:14 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,11 @@ validates :title, presence: true
 validates :content, length: { minimum: 250 }
 validates :summary, length: { maximum: 250 }
 validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+validate :validate, on: :create
 
   def validate
     if /(Won't Believe)|(Secret)|(Guess)|((Top) *[0-9])/.match(self.title) == nil
-      false
+      errors
     else
       true
     end
