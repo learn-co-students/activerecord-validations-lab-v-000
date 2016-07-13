be719815b7977a71545c3a081f8d013cf3c39ce6
DEREJE GEMECHU <dereje88@gmail.com>
2016-07-13 02:51:58 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+
+  #clickbait = lambda { |s| s.match title}
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validate  :is_clickbait?
+
+  TYPES = [/Won't Believe/i]
+
+  def is_clickbait?
+    if TYPES.none? { |p| p.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
+
 end
