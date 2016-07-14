1309dddac80f5250b6b4b8e188f85ee3868a3f4b
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-07-14 00:33:21 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :category, inclusion: {in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
+ 	validate :is_sufficiently_click_baity
+
+ 	def is_sufficiently_click_baity
+ 		patterns = [/Won't Believe/, /Secret/, /Top \d/, /Guess/]
+ 		if !patterns.any? {|pattern| pattern.match(self.title)}
+ 			errors.add(:title, "can't be in the past")
+ 		end
+ 	end
+
 end
