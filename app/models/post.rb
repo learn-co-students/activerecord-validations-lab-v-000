6797fd16463b23a372d096284b60bac4c0ba1d31
jbok4 <jbok4@aol.com>
2016-07-15 11:43:31 -0400

    validations complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} must be either Fiction or Non-Fiction" }
+  validate :clickbait?
+
+  CLICKBAIT = ["Won't Believe", "Secret", "Top
+  [number]", "Guess"]
+
+  def clickbait?
+    if title != nil
+      if CLICKBAIT.none? {|bait| title.include?(bait)}
+        errors.add(:title, "Title must contain either: Won't Believe, Secret, Top(enter number), or Guess")
+      end
+    end
+  end
+
+
 end
