9909371db01698cc443c338f80dcfd6afe849a12
Dom McKellar <dom.mckellar@gmail.com>
2016-07-31 02:47:39 -0700

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+    validates :phone_number, length: { is: 10 }
+    validates :name, uniqueness: true, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait?
+
+  def clickbait?
+    if [/(top)+\d/i, /won't believe/i, /secret guess/i].none? { |x| x.match(title) }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
