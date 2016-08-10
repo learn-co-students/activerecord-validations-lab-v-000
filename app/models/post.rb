7f0f83d0ef98ea91c7a6442032b6440a2b68dfed
jd2rogers2 <jd2rogers2@gmail.com>
2016-08-10 17:32:43 -0400

    donzo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 10}
+  validates :summary, length: {maximum: 50}
+  validates :category, inclusion: {in: ["Fiction"]}
+  validate :clickbait
+
+  STATEMENTS = [/Won't Believe/, /Secret/, /Guess/, /Top \d*/]
+
+  def clickbait
+    if STATEMENTS.none? {|state| state.match(title)}
+      errors.add(:title, "needs to be more clickbait-y")
+    end
+  end
 end
