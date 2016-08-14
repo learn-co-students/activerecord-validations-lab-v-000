a8592e8f797e928828c2761ca628e9cadd372396
miclp <michaelpeimani@gmail.com>
2016-08-13 22:35:18 -0700

    Add title click bait validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,26 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
+  validate :title_is_sufficiently_click_baity
+
+  def title_is_sufficiently_click_baity
+    errors.add(:title, "isn't click bait-y enough") if !click_bait?(self.title) #maybe just (title) as argument
+  end
+
+  def click_bait?(title)
+    cb = false
+    # return false if title is nil
+    return false if !title
+    if title.match(/Top \d*/) || title.match(/Won't Believe/) || title.match(/Secret/) || title.match(/Guess/)
+      cb = true
+    end
+    cb
+  end
+
+  def expiration_date_cannot_be_in_the_past
+    if expiration_date.present? && expiration_date < Date.today
+      errors.add(:expiration_date, "can't be in the past")
+    end
+  end
+
 end
