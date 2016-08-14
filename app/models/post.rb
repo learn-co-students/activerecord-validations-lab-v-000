ceec4f17a30ca131dcee32ce516a452cfac9f8c0
miclp <michaelpeimani@gmail.com>
2016-08-13 22:38:27 -0700

    Remove unused example code

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,10 +19,4 @@ class Post < ActiveRecord::Base
     cb
   end
 
-  def expiration_date_cannot_be_in_the_past
-    if expiration_date.present? && expiration_date < Date.today
-      errors.add(:expiration_date, "can't be in the past")
-    end
-  end
-
 end
