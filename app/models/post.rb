f6482b9c2702dcee3277127e8ce6b3367eb8acd0
Ivan Pilot <ivanpilot@gmail.com>
2016-08-25 02:41:55 +0200

    all tests passed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validate :title_is_valid
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+
+  private
+
+  def title_is_valid
+    if self.title && !title.match(/(Won't Believe)|(Secret)|(Top\d+)|(Guess)/)
+      errors.add(:title, "Title is not valid")
+    end
+  end
+
 end
