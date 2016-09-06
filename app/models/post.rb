ea931a7ef9176b1959c82eac75591d70fe6dd2a6
Caleb Faught <faughtcj@gmail.com>
2016-09-06 17:05:26 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'], message: "%{value} is not an accepted category." }
+  validate :clickbait?
+
+  CLICKBAIT = [
+    /Top [0..9]/i,
+    /Guess/i,
+    /Won't Believe/i,
+    /Secret/i
+  ]
+
+  def clickbait?
+    if CLICKBAIT.none? { |clickbait| clickbait.match title }
+      errors.add :title, "Not clickbait-y enough."
+    end
+  end
 end
