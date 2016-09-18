f7515d9a18bc106127d83f15cbec3975eba871d8
johnsontaiwo <josseytee@gmail.com>
2016-09-18 18:25:53 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait_title
+
+  
+CLICKBAIT = [/Won\'t Believe/i, /Secret/i, /Top[0-9]*/i, /Guess/i]
+ 
+
+  def clickbait_title
+    if CLICKBAIT.none? {|c| c.match title}
+      errors.add(:title, "is not clickbait-y enough")
+     end
+  end
 end
