55e6bea9851c8e37a08c2c9ec002503cf941e535
smithWEBtek <brad@smithwebtek.com>
2016-09-25 09:10:23 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,17 @@
 class AuthorsController < ApplicationController
+
+  def index
+    @authors = Author.all
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.create(:name => params[:name], :phone_number => params[:phone_number])
+  redirect_to authors_path(@author.id)
+  end
+
+  
 end
