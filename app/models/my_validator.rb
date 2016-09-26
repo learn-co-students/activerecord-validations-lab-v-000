9c947b4dcfd505ae51397bd16866a6816a869870
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-26 01:10:04 +0000

    removed the extra class validator

--- app/models/my_validator.rb
+++
@@ -1,9 +0,0 @@
-class MyValidator < ActiveModel::Validator
-
-  def validate(record)
-    record
-    num = [0..9]
-    record.title.starts_with?("Won't Believe", "Secret", "Top #{num}", "Guess")
-   end
-
-end
