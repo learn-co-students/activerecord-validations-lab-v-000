8ab7fba059b64997c1406651c0b9f0705bc863cb
ebanh <ebanhernandez@gmail.com>
2016-10-12 14:48:20 +0900

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+require 'pry'
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :click_bait?
+
+  def click_bait?
+    if [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i].none? { |w| w.match(title) }
+      errors.add(:title, "not clickbait")
+    end
+  end
+
 end
