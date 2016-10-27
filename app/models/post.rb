c8b4b94101e9f12b7436c11a2ca1d833c4b80213
Brad Watson <brad.watson.orlando@gmail.com>
2016-10-27 15:05:33 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :non_clickbait
+
+  def non_clickbait
+    if [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i].none? {|item| item.match title}
+      errors.add(:title, "invalid")
+    end
+  end
+
 end
