1de2feec93caedbc1a1e255b63abab411e489595
SnowboardTechie <bryan@snowboardteche.com>
2016-10-27 14:58:34 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :title, :click_baity
+
+  def click_baity
+    check = false
+    if self.title && self.title.include?("Won't Believe")
+      check = true
+    elsif self.title && self.title.include?("Secret")
+      check = true
+    elsif self.title && self.title.include?("Top ")
+      check = true
+    elsif self.title && self.title.include?("Guess")
+      check = true
+    else
+      errors.add(:title, "Not click-baity enough")
+    end
+    check
+  end
 end
