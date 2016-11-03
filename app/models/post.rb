a1dcf2277c3ee0bd34b1f24bcc2fc4ffe9f6757f
BriOD <briodonoghue@gmail.com>
2016-11-03 16:31:41 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} must be either Fiction or Non-Fiction" }
+
+  validate :clickbait_validator?
+
+  def clickbait_validator?
+    if title == nil || !title.match(/Won't Believe|Secret|Top\s\d+|Guess/)
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
