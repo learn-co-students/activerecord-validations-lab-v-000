c944888e189da848d4d8db48d334d22445150c6c
David Kennell <davidkennell0@gmail.com>
2016-11-20 15:53:07 +0000

    Almost done

---
+++ app/validators/my_validator.rb
@@ -0,0 +1,10 @@
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.title.include?("Won't Believe") ||
+           record.title.include?("Secret") ||
+           record.title.include?("Top [number]") ||
+           record.title.include?("Guess")
+      record.errors[:title] << 'Insufficiently clickbaity title!'
+    end
+  end
+end
