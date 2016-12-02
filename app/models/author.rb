09f8c8683cac12e4a3b3d4651e3ca448b0b6f2c1
johnram528 <johnram528@gmail.com>
2016-12-01 21:48:25 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,28 @@
 class Author < ActiveRecord::Base
+  validates_presence_of :name
+  validates :phone_number, length: { is: 10}
+  validates_uniqueness_of :name, :phone_number
 end
+
+
+#   it "is valid" do
+#     author = Author.new(name: "Caligula", phone_number: 5553054425)
+#     expect(author).to be_valid
+#   end
+
+#   it "is invalid with no name" do
+#     author = Author.new(phone_number: 5553054425)
+#     expect(author).to be_invalid
+#   end
+
+#   it "is invalid with a short number" do
+#     author = Author.new(name: "Caligula", phone_number: 555305442)
+#     expect(author).to be_invalid
+#   end
+
+#   it "is invalid when non-unique" do
+#     Author.create(name: "Caligula", phone_number: 5553054425)
+#     author = Author.new(name: "Caligula", phone_number: 5557890001)
+#     expect(author).to be_invalid
+#   end
+# end
