34139546f990cc2357890dec83a44f39ad58e447
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-12-19 02:21:12 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
+
+
+
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :clickbaity
+
+  def clickbaity
+    self.title.split.each do |word|
+    if ["Won't Believe","Secret","Top[number]","Guess"].include?(word)
+      errors.add(:clickbaity, "Not clickbaity")
+    end
+  end
 end
