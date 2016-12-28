54e78b9fd2dc218f3fd38bc9bbf395e710030574
WilliamMena <williammena1991@gmail.com>
2016-12-27 22:12:23 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true, inclusion: { in: ["Won't Believe", "Secret", "Guess"] }
+  include ActiveModel::Validations
+  validates_with MyValidator
+
+  validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
