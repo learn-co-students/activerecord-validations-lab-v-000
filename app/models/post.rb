6bf189f8461c993635858344eb4e5604622c4ea0
WilliamMena <williammena1991@gmail.com>
2016-12-27 22:22:30 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,11 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+
+  def validate(record)
+    unless record.title.include?("Won't Believe", "Secret", "Guess")
+      record.errors[:name] << "Not a valid title"
+    end
+  end
 end
