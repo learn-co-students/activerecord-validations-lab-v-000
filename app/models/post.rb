4a5fcf3392acff1c7bc1192c1f35277debd53a2f
Adam Lucia <lucia.adam@gmail.com>
2017-02-01 23:57:17 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
+require 'pry'
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+  validate :clickbait_title
+
+  def clickbait_title
+    allowable = ["Won't Believe", "Secret", "Top/\d+/", "Guess"]
+    place = false
+    allowable.each do |bait|
+      if self.title && self.title.include?(bait)
+        place = true
+      end
+    end
+
+    unless place
+      errors.add(:clickbait, "title isn't clickbait-y enough")
+    end
+  end
 end
