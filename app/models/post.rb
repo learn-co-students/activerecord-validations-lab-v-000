ab23fdf9aae0fb172e19a16e3254b434ca778219
Anna Lansfjord <lansfjord@gmail.com>
2017-02-17 23:23:27 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+
 class Post < ActiveRecord::Base
+validates :title, presence: true
+validates :content, length: { minimum: 250 }
+validates :summary, length: { maximum: 250 }
+validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+validate :validate, on: :create
+
+  def validate
+    if /(Won't Believe)|(Secret)|(Guess)|((Top) *[0-9])/.match(self.title) == nil
+      errors.add(:title)
+    end
+  end
+
+
 end
