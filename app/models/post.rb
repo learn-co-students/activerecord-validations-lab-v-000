f977a4a155bb848cef0f7f54e389ce913ad09631
Sarah <mrsmpython@gmail.com>
2017-03-11 14:20:08 -0500

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,16 +4,14 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validate :clickbait
-end
 
-
-
-def clickbait
-  if self.title != nil
-    title_arr = self.title.split(" ")
-    clickbait = ["Won't", "Believe", "Secret", "Top", "Guess"]
-    if title_arr - clickbait == title_arr
-      self.errors.add(:title, "please make it clickbait")
+   def clickbait
+    if self.title != nil
+      title_arr = self.title.split(" ")
+      clickbait = ["Won't", "Believe", "Secret", "Top", "Guess"]
+      if title_arr - clickbait == title_arr
+        self.errors.add(:title, "please make it clickbait")
+      end
     end
   end
 end
