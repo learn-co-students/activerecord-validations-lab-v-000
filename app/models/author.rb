00da7f3885fb946a022113b272af6be4e847c58e
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-03-28 13:31:29 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+    #name, phone_number
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :phone_number, numericality: { only_integer: true }, length: {is: 10}
 end
