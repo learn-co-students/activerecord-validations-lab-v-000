f486925f1f17234af32fa68166d3d4d9206829b2
bentonwong <bentonwong@gmail.com>
2017-04-05 21:14:19 -0700

    add validators

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
+require 'pry'
+
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    #binding.pry
+    words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    words.any? {|word| return if record.title.include?(word)} unless record.title.nil?
+    record.errors.add :title
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates_with MyValidator
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
 end
