60ca841fc6a027c3032a061f67d9c5825d475d66
github username <github email address>
2017-04-21 12:07:03 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 10}
+	validates :summary, length: {maximum: 20}
+	validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+	validate :clickbait?
+
+	def clickbait?
+		words = [/Won't Believe/i, /Secret/i, /Top \d*/i, /Guess/i]
+		if words.none? {|word| word.match title}
+			errors.add(:title, "Does not contain valid title.")
+		end
+	end
 end
