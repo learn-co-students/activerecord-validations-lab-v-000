0a51bb937e52ef35545b172ab007491353560bfc
Chenxi Wu <wuchenxi2013@gmail.com>
2017-04-23 21:23:21 +0200

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :summary, length: { maximum: 250 }
+  validates :content, length: { minimum: 250 }
+  validates :category, inclusion: ['Fiction', 'Non-Fiction']
+  validate :cb
+  def cb
+    if title==nil then errors.add(:title, "absence") 
+  elsif title.match("Won't Believe")==nil && title.match("Secret")==nil && title.match(/Top[0-9]/)==nil then errors.add(:title, "not click-baity") end
+  end
 end
