a7a22dcf08fd3cf514ed1b554359ff5434532c6f
anytimesoon <damien@delartigue.co.uk>
2017-04-27 17:38:37 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+	validate :clickbate_title
+
+	def clickbate_title
+		key_words = [/.*Won't Believe.*/, /.*Secret.*/, /.*Top \d+.*/, /.*Guess.*/]
+
+		if key_words.none? { |word| word.match(title) }
+			errors.add(:clickbate_title, 'needs more clickbate')
+		end
+
+	end
+
 end
