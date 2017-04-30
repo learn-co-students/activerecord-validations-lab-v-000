cef2bc6267b4d9bed26ad2ce086538e5f74773d4
Trai Compton <tlcompton2546@outlook.com>
2017-04-30 21:23:50 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,15 @@
 class Post < ActiveRecord::Base
   validates :title,
-    {
-      presence: true
-    }
+    { presence: true }
+
   validates :content,
-    {
-      length: {minimum: 250}
-    }
+    { length: {minimum: 250} }
+
   validates :summary,
-    {
-      length: {maximum: 250}
-    }
+    { length: {maximum: 250} }
+
   validates :category,
-    {
-      inclusion: { in: %w(Fiction Non-Fiction) }
-    }
+    { inclusion: { in: %w(Fiction Non-Fiction) } }
 
   validate :is_clickbait?
 
