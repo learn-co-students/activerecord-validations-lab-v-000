43936fcb00d63e210a74b29f889490033c347aa9
btcline04 <btcline04@gmail.com>
2017-05-24 22:38:14 -0400

    Done.

--- app/models/clickbait_validator.rb
+++ app/models/clickbait_validator.rb
@@ -2,7 +2,7 @@ class ClickbaitValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     bait = [/Won\'t Believe/, /Secret/, /Top \d+/, /Guess/]
     unless bait.any?{|b| value =~ b}
-      record.errors[:attribute] << (options[:message] || 'must be clickbaity')
+      record.errors[:attribute] << (options[:message] || 'must use a clickbaity title')
     end
   end
 end
