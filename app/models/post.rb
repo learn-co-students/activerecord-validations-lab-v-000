8af4965746e9ecb3ecde0e11a4537702a5ca7ffa
nadinesk <nadine.khattak@gmail.com>
2017-06-06 21:59:08 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,17 @@ class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :content, length: { minimum: 250}
     validates :summary, length: { maximum: 250}
-    validates :category, inclusion: { in: %w(Fiction, Non-Fiction, )}
-    validate :clickbaity?
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction, )}
+    validate :is_clickbaity?
 
 
-    def clickbaity?
-      if !(title.match(Regexp.union(["Won't Believe", "Secret", "Top", "Guess"]))
-        errors.add(:title, "title must be clickbait-y")
+    def is_clickbaity?
+    	a = title.to_s.downcase
+    	if  !(a.include? "won't believe") && !(a.include? "secret")  && !(a.include? "guess")  && !(a.include? "top") 
+    		errors.add(:title, "can't be greater than total value")
+    	end
+
+    	
     end
+
 end
