1265d173a195fc7cf44200a5716e4c640a0fd292
Menachem Hornbacher <me@mendel.tech>
2017-06-08 15:47:45 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
+class ClickbaitValidator < ActiveModel::Validator
+  def validate(record)
+    re = Regexp.union([
+      /Won't Believe/,
+      /Secret/,
+      /Guess/,
+      /Top \d+/
+    ])
+    unless record.title.to_s.match(re)
+      record.errors[:title] << 'More clickbait needed'
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates_with ClickbaitValidator
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction large), message: "%{value} is not a valid category" }
 end
