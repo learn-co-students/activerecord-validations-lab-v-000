af0eaf12953db411c048b275b57b47864712884b
MOSHE LEVI <Moishy@MOSHEs-MacBook-Air.local>
2017-06-09 15:13:18 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :category, inclusion: {in: ["Fiction","Non-Fiction"]}
+    validate :isClickbaity?
+
+    PHRASE_TO_SEARCH = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i,  /Guess/i]
+
+	def isClickbaity?
+		if PHRASE_TO_SEARCH.none? {|word| word.match title }
+			errors.add(:title, "Title not clickbait-y")
+		end	
+	end
 end
+
