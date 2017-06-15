daa23a8644f3c3f4530c651bed113b8d0612503e
machen2 <victoriamachen@gmail.com>
2017-06-15 02:02:50 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+  validate :is_clickbait?
+
+  def is_clickbait?
+    if self.title?
+      errors.add(:title, "must be click bait") unless self.title.include?("Won't Believe" || "Secret" || /Top \d/ || "Guess")
+    end
+  end
 end
