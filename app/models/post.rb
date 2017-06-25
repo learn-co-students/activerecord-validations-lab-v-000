a65e7de165e131141ef3e115084c531c7db27091
Pamela Bortnick <pbortnick@gmail.com>
2017-06-25 01:52:27 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+
+  validates :content, length: { minimum: 250 }
+
+  validates :summary, length: { maximum: 250 }
+
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :clickbait
+
+  OPTIONS = [/Won't Believe/, /Secret/, /Top[\d]/, /Guess/]
+
+  def clickbait
+    invalid = OPTIONS.none? { |option| option.match(title)}
+    if invalid
+      errors.add(:title, "title is not clickbait")
+    end
+  end
+
 end
