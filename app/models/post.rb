f3cfc3458ff2aa3b4cc359c4af4e5856498d31c5
Amanda George <github email address>
2017-07-03 10:30:00 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait
+
+  def clickbait
+    if self.title =~ /(?i)won't believe|secret|top|guess/
+    else
+      errors.add(:title, "must be clickbaity")
+    end
+  end
+
 end
