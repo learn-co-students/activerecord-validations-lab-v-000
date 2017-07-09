5009ad3bc99a25736ea98a4215973ee175897e9f
Luisa Scavo <luisascavo@gmail.com>
2017-07-09 12:35:00 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :is_clickbait?
+
+  CLICKBAIT = [
+    /Won't Believe/,
+    /Secret/,
+    /Top [0-9]/,
+    /Guess/
+  ]
+
+  def is_clickbait?
+    if CLICKBAIT.none? {|cb| cb.match(title)}
+      errors.add(:title, "Insufficient clickbaititude.")
+    end
+  end
 end
