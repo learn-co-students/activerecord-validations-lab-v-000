a089f02fbed512c4a54a960a476bda9fc769b7a2
curlywallst <nnoyes@comcast.net>
2017-07-12 10:46:13 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validate :is_clickable
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+
+  def is_clickable
+    if title != nil && !title.include?("Won't Believe" || "Secret" || "Top[number]" || "Guess")
+      errors.add(:title, "Not clickable")
+    end
+  end
 end
