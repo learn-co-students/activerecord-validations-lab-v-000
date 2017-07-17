ad39dc25dbfffa5513f51a5cbccfe829eebf331b
Will-Sauer <will@heueso.com>
2017-07-17 13:14:27 -0700

    finished

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: :true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :custom_validation
+    
+    def custom_validation
+        if [/Won't Believe\b/, /Secret\b/, /Top\s[0-9]*/, /Guess\b/].none? {|phrase|  phrase.match(title)}
+        errors.add(:title, "insufficiently salacious")
+        end
+    end
+
 end
+
+
+
+  
