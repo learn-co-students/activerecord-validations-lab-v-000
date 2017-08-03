4db1a144d95df86e2b997f2c26be666605d48621
Spencer Evans <spencevans@gmail.com>
2017-08-03 17:53:50 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :non_clickbait
+
+
+private
+
+  def non_clickbait
+    phrases = [/Won't Believe/, /Secret/, /Guess/, /Top \d+/]
+    unless phrases.any? { |phrase| phrase.match(self.title) }
+      errors.add(:title, "Must be click bait")
+    end
+  end
+
 end
