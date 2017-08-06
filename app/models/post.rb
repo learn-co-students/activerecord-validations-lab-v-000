37b8c14963bc6bb77639c941d05bbef577b6f669
Mel Chang <mel510@users.noreply.github.com>
2017-08-06 10:34:08 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates :summary, length: { maximum: 100 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait?
+
+  def clickbait?
+    valid_clickbait = [/Won't Believe/, /Secret/, /Top \d/, /Guess/]
+    if valid_clickbait.none? { |v| v.match title }
+      errors.add(:title, "not clickbaity enough!")
+    end
+  end
 end
