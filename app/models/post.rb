5632289e3682dcd950670b342f45efc1427c5835
Margarita Khodzka <mkhodzka@gmail.com>
2017-08-15 02:50:19 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :is_clickbait
+
+
+  def is_clickbait
+    if !title.nil? && !title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+      errors.add(:title, "Post is invalid if not clickbait")
+    end
+  end
 end
