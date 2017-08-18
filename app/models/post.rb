592a9ccab21dda23ff13d6511878610be9ec2fdd
streamedLine <dovidlwtn@gmail.com>
2017-08-18 16:20:22 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
+class ClickBaity < ActiveModel::Validator
+	def validate(record)
+		title = record.title || ""
+		valid = ["Won't Believe", "Secret", "Top #{/\d*/}", "Guess"].any? do |str|
+			title.include? str
+		end
+		if !valid 
+			record.errors[:clickbait] << "Title not clickbaity enough"
+		end
+	end
+
+end
+
 class Post < ActiveRecord::Base
+ 	include ActiveModel::Validations
+    validates_with ClickBaity
+	validates :title, presence: true
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+	
 end
