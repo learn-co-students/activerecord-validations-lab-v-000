141b7f9ac066738f5e3885940cff5e3c5baa9597
soheecho94 <soheecho94@gmail.com>
2017-08-23 22:59:25 +0900

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 80}
+  validates :summary, length: {maximum: 100}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :clickbait_y?
+
+
+  def clickbait_y?
+    if !(/Won't Believe|Secret|Top [0..9]|Guess/i).match(title) #i -> case sensitive
+      errors.add(:title, "this is not clickbait worthy!")
+    end
+  end
 end
