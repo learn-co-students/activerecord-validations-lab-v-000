d7c8cad7185fd038c3b1c001fb410a720b9213c8
gb23 <gregbenj@hotmail.com>
2017-08-23 14:10:49 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,20 @@ class Post < ActiveRecord::Base
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+
+    validate :is_clickbait?
+    
+      CLICKBAITS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]+/i,
+        /Guess/i
+      ]
+    
+    def is_clickbait?
+        if CLICKBAITS.none? { |bait| bait.match title }
+           errors.add(:title, "this is not clickbait!")
+        end
+    end
+    
 end
