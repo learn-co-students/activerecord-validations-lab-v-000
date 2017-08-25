d9516646b32fe46e984c4b7905a8d56c3e4f07a1
Parv Kumar <pkumar@trinity.edu>
2017-08-25 18:47:06 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :title_category
+
+
+  def title_category
+    if self.title == true
+      if self.title.include?("Won't Believe")|| self.title.include?("Secret")||self.title.include?("Guess")||self.title.include?("Top")
+        return true
+      else
+        errors.add(:title, "title must include clickbait-y terms")
+      end
+    end
+  end
 end
