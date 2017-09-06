55066b1a25ae9e1ec3d3be37cb7d9cd6f67c084c
kaisa <kaisa.piipari@gmail.com>
2017-09-06 14:35:06 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
+
+class MyValidator < ActiveModel::Validator
+
+    VALID_TITLE_WORDS = ["Won't Believe", "Secret", "Top", "Guess"]
+
+    def validate(post)
+        clickbatable = false
+        VALID_TITLE_WORDS.each do |word|
+            if post.title != nil && post.title.include?(word)
+                clickbatable = true
+            end
+        end
+        post.errors[:title] << 'Title needs to be clickbatable!' unless clickbatable
+    end
+end
+
 class Post < ActiveRecord::Base
+
+    include ActiveModel::Validations
+    validates_with MyValidator
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
+
 end
