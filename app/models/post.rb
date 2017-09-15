d1db00a656666ff5718a7dc652a71cdf854d2cc0
Jelo Yambao <jelolearns@gmail.com>
2017-09-15 20:37:59 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion:  { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not valid" }
+  validate :clickbait
+
+  def clickbait
+    errors.add(:title, "Please use a clickbaity title") if !title || !title.downcase.match(/won't believe|secret|top \d|guess/)
+  end
 end
