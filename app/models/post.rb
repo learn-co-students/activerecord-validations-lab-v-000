da8018f4dc98475d9a36dd2487364e5dc39e490b
Tyler <tbuchhei@alumni.nd.edu>
2017-09-16 21:45:01 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w{Fiction Non-Fiction}}
+  validate :click_bait
+
+  def click_bait
+    acceptable = [/Won't Believe/, /Secret/, /Top \d+/, /Guess/]
+    unless acceptable.any? {|cb| title.index(cb) if title}
+      errors.add(:click_bait, "Needs more click bait")
+    end
+  end
+
 end
