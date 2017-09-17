740aac77f5c8894be10546a27976125a6621caf1
Villy Siu <villysiu@gmail.com>
2017-09-17 05:22:06 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
+
+
 class Post < ActiveRecord::Base
+
+
+  validates :title, presence: true
+  validates :content, { :length => { :minimum => 250 } }
+  validates :summary, { :length => { :maximum => 250 } }
+
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
+
+  validate :title_keywords
+
+  private
+
+  def title_keywords
+    if !title.nil?
+      keywords = ["Won't Believe", "sSecret", "Top
+  [number]", "Guess"]
+      if !keywords.any? {|keyword| title.include? keyword}
+        errors.add(:title, "must contain keywords")
+      end
+    end
+  end
 end
