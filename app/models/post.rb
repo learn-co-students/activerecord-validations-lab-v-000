753a7d37598ab2cfff9904b29891ff923df28d85
Tony <github email address>
2017-10-13 13:02:32 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :is_clickbaity?
+
+    ARR = ["Won't Believe", "Top", "Secret", "Guess"]
+
+    def is_clickbaity?
+        if !ARR.any? { |w| self.title =~ /#{w}/ }
+            errors.add(:title, "make it more clickbaity!")
+        end
+    end
 end
