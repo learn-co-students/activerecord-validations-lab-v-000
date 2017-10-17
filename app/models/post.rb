27a18cf803809e3edfc2bf28fe4f9e0f75ca5102
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-10-17 14:05:19 -0700

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
+require 'pry'
+
 class Post < ActiveRecord::Base
+  def clickbaity?
+    clicky = ["Won't Believe", "Secret", "Top
+    [number]", "Guess"]
+
+    unless !self.title.nil? && clicky.any? {|word| self.title.include?(word)}
+      errors[:title] << 'Title not Clickbaity Enough'
+    end 
+  end
+
+  validates :title, presence: true
+  validate :clickbaity?
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
