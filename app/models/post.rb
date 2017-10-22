e48d1a7f5ac89365a33fe107e399711a9388692e
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-10-22 00:51:00 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+class MyValidator < ActiveModel::Validator
+  def validate(post)
+    unless post.title == nil || post.title.match(/Believe|Won't|Secret|Top[number]|Guess/)
+      post.errors[:title] << 'Needs a clickbait-y title!'
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates_with MyValidator
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%(value) is not a valid category"}
 end
