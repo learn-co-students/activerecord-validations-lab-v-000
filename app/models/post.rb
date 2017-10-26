aed234a01923ac370c7e10ec7ee17288aee2fb8f
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-26 10:44:22 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
+  
+  def is_clickbait?
+    clickbait = /top \d+|won't believe|secret|guess/i
+
+    unless title.nil?
+      errors.add(:title, "must be clickbait-y!") unless title.match(clickbait)
+    end
+  end
+
 end
