10d6d03bbccd781647f844dceb161d92aa537d17
Igor Eskin <github email address>
2017-10-28 22:19:15 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ActiveRecord::Base
 
-  CLICKBAITS = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+  CLICKBAITS = ["Won't Believe", "Secret", "Top ", "Guess"]
 
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validate :is_clickbait?
 
 
