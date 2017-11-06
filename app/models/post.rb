1a05fe7328c254ff1a999b5137ce8628475be3da
Seb Gertz <sebgertz@gmail.com>
2017-11-06 04:25:54 +0000

    add

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validate :title_is_clickbaity
 
-
+  private
 
   def title_is_clickbaity
     if title.nil? || !title.include?("Won't Believe" || "Secret" || /Top \d+/ || "Guess")
