62716687d4087155344d4d480dc8a73308a44c7f
Christine Tran <christinetran825@gmail.com>
2017-11-13 05:35:19 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: {maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
+
+  CLICKBAIT_LISTS = ["Won't Believe", "Secret", "Top", "Guess"]
+
+
+  def is_clickbait?
+    unless title == nil
+      if CLICKBAIT_LISTS.none? { |phrase| title.include?(phrase)}
+        errors.add(:title, "Should Be Clickbait")
+      end
+    end
+  end
+
 end
