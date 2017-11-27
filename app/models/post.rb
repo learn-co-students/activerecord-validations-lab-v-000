6379458fa92f4006b618b04966d321f0730a96dc
kyleblee <hello.kylelee@gmail.com>
2017-11-26 22:25:00 -0500

    lab complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbaity
+
+  def clickbaity
+    if title == nil
+      false
+    else
+      if title.include?("Won't Believe") || title.include?("Secret") || title.match(/Top\s\d/) || title.include?("Guess")
+        true
+      else
+        errors.add(:title, "not clickbaity enough")
+      end
+    end
+  end
 end
