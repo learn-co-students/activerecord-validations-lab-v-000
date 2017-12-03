045d1526cbd1efb1ee3ae021bd4b0fe44a07a9bc
Ben <baschenbrenner@hotmail.com>
2017-12-03 20:07:54 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
+    
+    
+    validate :clickbait, on: :create
+ 
+  def clickbait
+      indicators = ["Won't Believe", "Secret", "Top", "Guess"]
+      if title != nil
+         if indicators.map {|indicator| title.include?(indicator)}.include?(true)
+        else
+          errors.add(:title, "must include clickbait terms")
+         end
+      end
+  end
+ 
+  
+    
 end
