910f0ec37baa6bad52b5986f73bb3003ec789a8e
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-12-11 20:46:11 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250} 
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbaity?
+    
+    
+    
+        
+    CLICKBAIT = [
+        /Won't Believe/i, 
+        /Secret/i, 
+        /Top [0-9]*/i,
+        /Guess/i 
+        ]
+    
+     def clickbaity?
+        if CLICKBAIT.none? { |bait| bait.match title }
+      errors.add(:title, "Title must be clickbait")
+    end
+  end
 end
+
+
+
