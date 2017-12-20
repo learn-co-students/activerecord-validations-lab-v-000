05161eeb4ccb9fabc20c953427bb918c233f44e6
Justin Tooley <jstooley823@gmail.com>
2017-12-20 16:46:32 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates_with MyValidator
+end
+
+class MyValidator < ActiveModel::Validator
+  def clickbate(post)
+    binding.pry
+
+  end
 end
