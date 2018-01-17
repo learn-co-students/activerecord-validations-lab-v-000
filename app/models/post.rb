3137792eb51488f3c17e3dd81cf5964762f6d3d3
Michael Mirzakandov <mikemirzak@aol.com>
2018-01-17 16:42:51 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
-class Post < ActiveRecord::Base
+class PostTitleValidator < ActiveModel::Validator
+  def validate(record)
+    unless !!record.title && record.title.match(/\b(?:Won't Believe|Secret|Guess|Top [0-9]*)\b/)
+      record.errors[:title] << 'Title must be clickbait-y!'
+    end
+  end
 end
+
+ class Post < ActiveRecord::Base
+  validates_with PostTitleValidator
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+ end
