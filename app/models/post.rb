9abfd66d1cb090d3fa94a19f5a45ccc06360d365
PRbsas <paularamirez0316@gmail.com>
2018-01-17 12:42:42 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,15 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: { minimum: 100 }
-  validates :summary, length: { maximum: 100 }
-  validates :category, inclusion: { in: %w(Fantasy Romance Adventure Fiction) }
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validate :is_clickbait?
 
   CLICKBAIT_PATTERNS = [
     /Won't Believe/i,
-    /Secret/i
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
   ]
 
   def is_clickbait?
