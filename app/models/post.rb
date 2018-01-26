6e5d023a046369cd48f30fc93a356e1ff77dfef5
Jacqueline Lee <anginblu@gmail.com>
2018-01-26 19:45:51 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, inclusion: { in: ["Won't Believe", "Secret", "Top [number]", "Guess"] }
+  validates :content, presence: true, length: { :minimum => 250 }
+  validates :summary, presence: true, length: { :minimum => 250 }
+  validates :category, presence: true, inclusion: { in: ["Fiction", "Non-Fiction"] }
 end
