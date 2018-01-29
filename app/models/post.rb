4a21ea2698bcfe79594d064547c7c295a9706413
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-01-29 22:52:25 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Non-Fiction Fiction), message: "Not a valid genre" }
+  validate :clickbait
+
+  def clickbait
+    clickbait = ["Won't Believe", "Secret", "Guess", "Top"]
+    if self.title
+      is_clickbait = clickbait.any? do |bait|
+        self.title.include?(bait)
+      end
+    end
+   errors.add(:title, "Must have clickbait title") unless is_clickbait
+ end
+
 end
