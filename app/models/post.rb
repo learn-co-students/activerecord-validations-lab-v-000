fb9a741afe8482c5507f91ec951078080801d9c7
Hunter Kleindl <hkleindl@gmail.com>
2018-02-01 18:57:29 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :click_bait
+
+  def click_bait
+    if title && !title.match(/\b(Won't|Believe|Secret|Top \d+|Guess)\b/)
+      errors.add(:title, "more clickbait-y")
+    end
+  end
 end
