033ecf1ecb3f8c1a488c184bc88425abc1ee4883
bretburau <github email address>
2018-02-02 13:12:18 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    # binding.pry
+    if record.title == nil
+      record.errors[:name] << "Must have a title"
+    else
+      unless record.title.include?("Won't Believe")
+        record.errors[:name] << "Must be clickbait"
+      # "Won't Believe", "Secret", "Top[number]", "Guess"
+      end
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum:250}
+  validates :category, inclusion: { in: %w(Fiction Non-fiction)}
+  include ActiveModel::Validations
+  validates_with MyValidator
 end
