7d91a710b1c1503cf57dcc4405cd255a77d641e7
Randall Robinson <keith_adams007@yahoo.com>
2018-02-12 19:26:12 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :click_bait
+
+
+
+  def click_bait
+    if self.title != nil
+      errors.add(:title, "is not attractive enough") unless self.title.include? "Won't Believe"
+    end
+  end
 end
