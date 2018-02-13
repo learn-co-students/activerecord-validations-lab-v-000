e889ef43ad2a50a18f430bc799811e5452188f5e
Alex <github email address>
2018-02-12 22:42:13 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
   CLICKBAIT_TITLES = ["Won't Believe", "Secret", "Top[number]", "Guess"]
 
   validates :title, presence: true
-  validates :content, length: { minimum: 10}
-  validates :summary, length: { maximum: 100  }
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 #implement custom validation
   validate :clickbait #validate not plural
@@ -15,5 +15,24 @@ class Post < ActiveRecord::Base
         errors.add(:title, "must be clickbait")
       end
   end
+
+
+#below is a second example of a custom validation
+#i believe the CLICKBAIT_PATTERNS array uses regex
+
+  #validate :is_clickbait?
+
+  #CLICKBAIT_PATTERNS = [
+  #  /Won't Believe/i,
+  #  /Secret/i,
+  #  /Top [0-9]*/i,
+  #  /Guess/i
+  #]
+
+  #def is_clickbait?
+  #  if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+  #    errors.add(:title, "must be clickbait")
+  #  end
+  #end
 
 end
