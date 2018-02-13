0fd275a7c1953a764c4c9094d8876343f5c60425
Brittany Morris <brittmmendez@gmail.com>
2018-02-13 16:28:10 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),}
-  validate :clickbait_y?, on :title
+  validate :clickbait_y?
 
   def clickbait_y?
     clickbaits = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
