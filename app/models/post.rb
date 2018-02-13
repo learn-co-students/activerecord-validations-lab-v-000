533f783ea2184c58b70b2d1030d81c4e5cf22a01
Brittany Morris <brittmmendez@gmail.com>
2018-02-13 16:25:30 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,13 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),}
+  validate :clickbait_y?
 
+  def clickbait_y?
+    clickbaits = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+
+    if clickbaits.none? { |clickbait| clickbait.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
