efee9a69713c0b926f07be5d48e6481f0c3cfa68
PJ Frias <peejfrias@gmail.com>
2018-02-20 19:41:44 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+require 'pry'
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbait
+
+  @@clickbait_phrases = ["Won't Believe", "Secret", "Top (/[0..9]*/)", "Guess"]
+
+  def clickbait
+    if @@clickbait_phrases.none?{|phrase|title.include?(phrase) if !!title}
+      errors.add(:title, "must contain clickbait phrase")
+    end
+  end
 end
