6daea22e92ede7d15d77df17f07de59311e29980
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-26 22:07:03 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
+class TitleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value.match? /(Secret|Guess|Won\'t Believe|Top)/
+      record.errors[attribute] << (options[:message] || "is not clickbaity title")
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true, title: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
