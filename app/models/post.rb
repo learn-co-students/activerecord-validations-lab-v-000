b958adfc611cb44d94c56f250f38e66bf6ee5cfa
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-02-28 21:54:38 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates_inclusion_of :category, :in => %w( Fiction Non-Fiction )
+    # validates :title, inclusion: true, if: '"Won't Believe"|| "Secret"|| "Top[number]"|| "Guess".present?'
+    # validates :title, inclusion: %w("Won't Believe", "Secret", "Top[number]", "Guess")
+      validate :clickbait
+    
+
+     def clickbait
+           unless title =~( /Won't Believe|Secret|,|Top [0-9]|,Guess/)
+               errors.add(:title, "title invalid")
+           end
+     end
+        
 end
