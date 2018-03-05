c9f002b173b66b315afbe95a7165d7c02e3dbbc2
catalanowebdevelopment <catalano.work@gmail.com>
2018-03-05 13:59:04 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates(:content, { length: { minimum: 250 }})
+    validates(:summary, { length: { maximum: 250 }})
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :title_must_be_clickbait
+    
+    def title_must_be_clickbait
+        clickbait = [/Won't Believe/, /Secret/, /Top/, /Guess/]
+        clickbait.each do |bait|               
+           if self.title == nil || !self.title.match(bait)
+                errors.add(:title, "Title must be present and clickbait")
+           else
+               return true
+           end
+        end
+    end
+    
 end
