bba36e46acff47faff5c8020cfe4821cdec848c3
jacobsilver2 <jacobsilver2@mac.com>
2018-03-12 03:43:21 -0400

    post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 10}
-  validates :content, length: {maximum: 100}
+  validates :content, length: {minimum: 100}
+  validates :summary, length: {maximum: 100}
+  validates :category, 
 end
+
+
