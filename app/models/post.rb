60f6cbe58ff4318054b4ac88d4d88ff204c75405
jacobsilver2 <jacobsilver2@mac.com>
2018-03-12 04:00:39 -0400

    almost done

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,20 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 100}
   validates :summary, length: {maximum: 100}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :is_clickbait?
+
+  CB = [
+    /Won't Believe/,
+    /Secret/,
+    /Top [0-9]*/,
+    /Guess/
+  ]
 
   def is_clickbait?
-    
+    if CB.none? { |p| p.match title}
+    end    
   end
+
 end
 
 
