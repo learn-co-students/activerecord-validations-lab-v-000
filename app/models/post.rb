f28e63fdfbde6552ac87c81b1a5ab08242b9a505
jacobsilver2 <jacobsilver2@mac.com>
2018-03-12 03:37:43 -0400

    what

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: {minimum: 10, maximum: 100}
+  validates :content, length: {minimum: 10}
+  validates :content, length: {maximum: 100}
 end
