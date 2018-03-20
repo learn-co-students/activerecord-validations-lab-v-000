56e036ed9b8fe9499a1046c96e7164246f0f10ff
github username <github email address>
2018-03-20 09:12:06 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :title_must_be_click_bait
+
+  private
+
+  def title_must_be_click_bait
+    unless title && (title.include?("Won't Believe") || title.include?("Secret") || title.include?("Guess") || title.include?("Top \d"))
+      errors.add(:title, "Must be sufficiently click-baity")
+    end
+  end
+
 end
