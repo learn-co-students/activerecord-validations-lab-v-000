3ee32c9cf4149ea688d84581ad79f795e1e18015
EC2 Default User <ec2-user@ip-172-31-15-206.us-east-2.compute.internal>
2018-03-25 07:07:50 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validate :clickbait_title
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    
+    def clickbait_title
+        if title.present? && !(title.include?("Won't Believe") || title.include?("Secret")  || title.include?("Guess") || title.match(/Top\s\d*/))
+              errors.add(:title, "must be clickbait-y")
+        end
+    end
 end
