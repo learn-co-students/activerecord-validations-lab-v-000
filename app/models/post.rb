09d309946d2e6008b0a317bc60ed42937eaff1a3
Emily Howard <howard.emilya@gmail.com>
2018-03-27 02:04:04 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
+
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :click_bait?
+
+  def click_bait?
+    errors.add(:title, "Not clickbait-y") unless title.to_s.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+  end
 end
