12b08a2d5d62a71d946d691660de10e727135fc8
Rygel-XVI <micha232@umn.edu>
2018-03-27 22:12:08 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbaity_enough?
+
+    WORDS = [/Won\'t Believe/, /Secret/, /Guess/, /Top\s\n/]
+
+  def is_clickbaity_enough?
+    if WORDS.none? {|word| word.match(title) }
+      errors.add(:title, "not clickbaity enough")
+    end
+  end
+
 end
