fc59e903062ce28feb9df268ed595e04e3a67c9b
Allyson Wesman <allysonwesman@gmail.com>
2018-03-31 13:25:08 -0700

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    unless self.title =~ /(Won't)|(Secret)|(Top \d)|(Guess)/
+      errors.add(:title, "Needs to be clickbait")
+    end
+  end
+
 end
