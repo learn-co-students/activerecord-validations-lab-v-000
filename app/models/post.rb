5512f0591e9c61d2662f0b428be79301d31fb16b
ruthmesfun <ruth.mesfun@gmail.com>
2018-04-01 14:41:54 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
-class Post < ActiveRecord::Base
+class ClickBaitValidator < ActiveModel::Validator
+    def validate(record)
+        if record.title.downcase.split(' ').any?{|word| word == "top" || word == "believe" || word == "secret" || word == "guess" || word == "true"}
+            false
+        end 
+    end
 end
+
+
+class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in:["Fiction","Non-Fiction"]}
+    
+    validates_with ClickBaitValidator
+end
+
