9c4e9041b2d231f3a2add183386aa3dd94f0886c
Tyler Maxwell <tmax818@mac.com>
2018-04-05 05:07:13 -0700

    init commit.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
+class TitleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /You Won't Believe These True Facts/
+      record.errors[attribute] << (options[:message] || "is not clickbait-y")
+    end
+  end
+end
+
+
+
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true, title: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+
+
 end
