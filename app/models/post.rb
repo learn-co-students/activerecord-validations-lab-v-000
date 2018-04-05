da1503bf61ad0e0db9a3caa10f4d3452b887a875
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-05 00:14:30 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+class TitleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless !value || value.downcase =~ /won't believe|secret|guess|top \d+/
+      record.errors[attribute] << false
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true, title: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
