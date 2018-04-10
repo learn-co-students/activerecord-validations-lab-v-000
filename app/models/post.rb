b65474dfcd0eff6687be0018ca61cfea87d7f2de
nptravis <nptravis@yahoo.com>
2018-04-10 21:27:29 +0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 10}
+	validates :summary, length: {maximum: 100}
+	validates :category, inclusion: {in: %w(Fiction Non-fiction)}
+	validate :clickbait
+
+	private 
+
+	def clickbait
+		unless title != nil && title.match(/\bWon't Believe\b|\bTop\b|\bSecret\b|\bGuess\b/)  
+			errors.add(:title, "title not clickbaitable enough")
+		end
+	end
+
 end
+
+# "Won't Believe", "Secret", "Top
+# [number]", or "Guess"
