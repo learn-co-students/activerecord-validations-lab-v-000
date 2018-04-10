28c42292aec011ff87e330ed902d23997992bed3
badlychadly <chadmontoya21@yahoo.com>
2018-04-10 18:00:19 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait?
+
+
+  def clickbait?
+    
+    if self.title 
+      
+      return !!/Won't Believe|Secret|Guess/.match(self.title)
+      
+      
+    end
+  end
 end
