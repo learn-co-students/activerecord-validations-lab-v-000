d25840ef9782ee59e4256155e1c7d5d1074fa1a5
Morgan Fudge <mfudge92@gmail.com>
2018-04-11 16:14:41 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, format: { with: %r(([\s\S]*(Guess | Secret | Won't Believe | Top [0-9])[\s\S]*)) }
+  validates :content, { length: { minimum: 250 } }
+  validates :summary, { length: { maximum: 250 } }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
