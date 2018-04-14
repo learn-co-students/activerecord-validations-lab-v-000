2af4e40f6d184ad0eeca4226b9744fdf023c6715
Robert Hughes <roberts.ginger.email@gmail.com>
2018-04-14 12:10:47 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :clickbaity
+
+  def clickbaity
+    unless title && (title.include?("Won't Believe") || title.include?('Secret') || title.match(/top \d/) || title.include?('Guess'))
+      errors.add(:title, 'is not clickbaity')
+    end
+  end
 end
