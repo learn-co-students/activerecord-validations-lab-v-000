f05923f51fb859380529cb48e2371725dd30083d
Susan Fowler <infopro94@gmail.com>
2018-04-16 10:59:40 -0500

    completed lab tests passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,22 @@
 class Post < ActiveRecord::Base
-# 	All posts have a title
-# Post content is at least 250 characters long
-# Post summary is a maximum of 250 characters
-# Post category is either Fiction or Non-Fiction 
-# This step requires an inclusion validator, which was not
-#  outlined in the README lesson. You'll need to refer to
-#   the Rails guide t
+	validates :title, presence: true
+	validates :content, length: { minimum: 250 }
+	validates :summary, length: { maximum: 250 }
+	validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} must be Fiction or Non-Fiction" }
+    validate :is_clickbait?
+
+
+    CLICKBAIT_PATTERNS = [ #constant outlines conditions
+     /Won't Believe/i,
+     /Secret/i, 
+     /Top 10/i, 
+     /Guess/i 
+ ]
+    
+    def is_clickbait?
+    	if CLICKBAIT_PATTERNS.none? { |phr| phr.match title } #regex
+    		errors.add(:title, "must be clickbait") #ActiveRecord guide section 6.2
+    	end
+    end
 end
