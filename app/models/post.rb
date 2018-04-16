a12ebc42722f9c46c967a47f7abaac604ffa264e
Susan Fowler <infopro94@gmail.com>
2018-04-16 10:16:37 -0500

    entered validations for Author model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+# 	All posts have a title
+# Post content is at least 250 characters long
+# Post summary is a maximum of 250 characters
+# Post category is either Fiction or Non-Fiction 
+# This step requires an inclusion validator, which was not
+#  outlined in the README lesson. You'll need to refer to
+#   the Rails guide t
 end
