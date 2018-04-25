08b4752a85020a558382910621b78dd7596f1b56
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-25 23:25:03 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,35 @@
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    if record.title != nil
+      if record.title.include? "Won't Believe"
+      elsif record.title.include? "Secret"
+      elsif record.title.include? "Guess"
+      elsif record.title.include? "Top"
+        x = record.title.split("Top")
+        counter = false
+        x.each do |int|
+          if int.to_i != nil
+            counter == true
+          end
+        end
+        if counter == false
+          record.errors[:title] << 'clickbait-y'
+        end
+      else
+        record.errors[:title] << 'clickbait-y'
+      end
+    else
+      record.errors[:title] << 'No title given'
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  include ActiveModel::Validations
+  validates_with MyValidator
 end
