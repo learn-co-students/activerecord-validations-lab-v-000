5332a62d24566084b4ded5816ce8a51c5d9a3e9b
roitblatari <roitblatari@gmail.com>
2018-04-26 23:07:48 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,9 +18,9 @@ class Post < ActiveRecord::Base
   
   validates :category, inclusion: { in: %w(Fiction  Non-Fiction)}
   #validates_with FictionValidator
-    validate :clickbait
+  validate :clickbait
 
-    TITLE_PATTERNS = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+  TITLE_PATTERNS = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
 
   def clickbait 
     if TITLE_PATTERNS.none?{ |e| e.match(self.title) } # true or false
@@ -29,24 +29,3 @@ class Post < ActiveRecord::Base
   end
 end
 
-# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-# class Invoice < ApplicationRecord
-#   validate :expiration_date_cannot_be_in_the_past,
-#     :discount_cannot_be_greater_than_total_value
-
-#   def expiration_date_cannot_be_in_the_past
-#     if expiration_date.present? && expiration_date < Date.today
-#       errors.add(:expiration_date, "can't be in the past")
-#     end
-#   end
-
-#   def discount_cannot_be_greater_than_total_value
-#     if discount > total_value
-#       errors.add(:discount, "can't be greater than total value")
-#     end
-#   end
-# end
-# CUSTOM VALIDATIONS
-# Finally, add a custom validator to Post that ensures the title is sufficiently clickbait-y. If the title does not contain "Won't Believe", "Secret", "Top [number]", or "Guess", the validator should return false.
-
-# For future projects, it's a good idea to skim the list of available validation options. It will give you a sense of what's available and help with creating validations for your own model objects.
