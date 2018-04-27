52fa027f1baa80f8636bd0e9f85d9bbc8435e0c9
roitblatari <roitblatari@gmail.com>
2018-04-26 23:07:16 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,40 +1,45 @@
 
-class FictionValidator < ActiveModel::Validator
-  def validate(record)
-    # binding.pry
-    if record.category != "Non-Fiction" && record.category != "Fiction" 
-      record.errors[:category] << "is"
-    end
-  end
-end
- 
+# class FictionValidator < ActiveModel::Validator
+#   def validate(record)
+#     # binding.pry
+#     if record.category != "Non-Fiction" && record.category != "Fiction"
+#       record.errors[:category] << "this needs to be Non-Fiction or Fiction"
+#     end
+#   end
+# end
+
 
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations 
+  include ActiveModel::Validations
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  # validates :category, inclusion: { in: %w(Fiction  Non-Fiction)}
-  validates_with FictionValidator 
+  
+  
+  validates :category, inclusion: { in: %w(Fiction  Non-Fiction)}
+  #validates_with FictionValidator
+    validate :clickbait
 
-  # def clickbait
-  #   if title == 
-      
-  #   end
-    
-  # # end
+    TITLE_PATTERNS = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+
+  def clickbait 
+    if TITLE_PATTERNS.none?{ |e| e.match(self.title) } # true or false
+      errors.add(:title, "needs to be one of this = Won't Believe - Secret - Top [number] - Guess")
+    end
+  end
 end
+
 # /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 # class Invoice < ApplicationRecord
 #   validate :expiration_date_cannot_be_in_the_past,
 #     :discount_cannot_be_greater_than_total_value
- 
+
 #   def expiration_date_cannot_be_in_the_past
 #     if expiration_date.present? && expiration_date < Date.today
 #       errors.add(:expiration_date, "can't be in the past")
 #     end
 #   end
- 
+
 #   def discount_cannot_be_greater_than_total_value
 #     if discount > total_value
 #       errors.add(:discount, "can't be greater than total value")
