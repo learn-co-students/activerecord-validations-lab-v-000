cae5b7ec1bfdf8b3f54c096ae4a89d3fd5a6df36
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-01 14:59:52 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  def invalid?
+    unless self.title.include? ("Won't Believe") || self.title.include? ("Secret") || self.title.include? (/Top\s\d*\s/) || self.title.include? ("")
+    true
+  end
+
+  def invalid?
+    ["Won't Believe", "Secret", /Top\s\d*\s/, "Guess"].any? { |keyword| self.title.include? keyword}
+
+
 end
