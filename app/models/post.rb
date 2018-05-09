6b28bf029a20fd3cfaadca3c5b1bc5aac6b1f5c8
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-05-09 02:54:18 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,14 +2,14 @@
 
 
 class Post < ActiveRecord::Base
-    validates :title, presence: true
+    validate :is_clickbaity?
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: [ "Fiction", "Non-iction"], message: "please include fiction or non-fiction for the category" }
-    validate :is_clickbaity?
+
     
       def is_clickbaity?
-          if [ "Won't Believe", "Secret", "Top [number]", "Guess"].any? { |i| title.inclue? i }
+          if title == nil || [ "Won't Believe", "Secret", "Top [number]", "Guess"].none? { |i| title.include?(i) }
             errors.add(:title, 'Need a title starting with some more clickbait please!')
           end
       end
