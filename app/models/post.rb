c293dd423f4d49412fd4f0bd6604cda17480bf15
Aly Alidina <aly_alidina@hotmail.com>
2018-05-13 03:07:58 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+validates :title, presence: true
+validates :content, presence: true, length: {minimum: 250}
+validates :summary, presence: true, length: {maximum: 250}
+validates :category, inclusion: { in: %w(Fiction  Non-Fiction)}
+validate :clickbait_y?
+
+  def clickbait_y?
+    # Make a constant variable, use regex to store the clickbait titles
+    # the i makes it case sensitive
+    clickbait = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+    if clickbait.none? { |t| t.match title }
+      errors.add(:title, "Title doesn't contain matching text")
+    end
+  end
 end
+
+#rspec ./spec/models/post_spec.rb
