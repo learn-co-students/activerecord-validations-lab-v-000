685664b907f472de3355ae8287b8d76b02b8c084
Kaitlin Kelly <kk504408@gmail.com>
2018-05-14 02:29:27 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,19 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
-  validate :title_must_be_clickbaity
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
 
-  def title_must_be_clickbaity
-    clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
-    if self.title == nil
-      errors.add(:title, "must have title")
-    elsif clickbait.any? { |s| self.title.include? s} == false
-      errors.add(:title, "must be more clickbaity")
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+    ]
+
+  def is_clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
     end
   end
 
