e10150141284cdd962c85a40456bed4764339659
Kaitlin Kelly <kk504408@gmail.com>
2018-05-14 02:25:41 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,17 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: {minimum: 10}
-  validates :summary, length: {maximum: 100}
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
+  validate :title_must_be_clickbaity
+
+  def title_must_be_clickbaity
+    clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+    if self.title == nil
+      errors.add(:title, "must have title")
+    elsif clickbait.any? { |s| self.title.include? s} == false
+      errors.add(:title, "must be more clickbaity")
+    end
+  end
+
 end
