033d44a0041a281336a07bc3ae06fb74e9d6ff75
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-15 06:42:06 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :title_contains_click_bait
+
+  def title_contains_click_bait
+    if title.include?("Won't Believe")
+      true
+    else
+      false
+    end
+  end
 end
