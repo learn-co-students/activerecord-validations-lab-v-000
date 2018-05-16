b7a1b2b51e894bb85d4294e8b7b9cd8f44c5c601
jokerdelta <jokerdelta@yahoo.com>
2018-05-15 22:05:40 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  # validate :title_must_contain_click_bait
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :not_clickbait
+
+  def not_clickbait
+    unless title.present? && title == "Won't Believe" || "Secret" || "Top[number]" || "Guess"
+      errors.add(:title, "Title needs more click-bait!!")
+    end
+  end
+
+
 end
