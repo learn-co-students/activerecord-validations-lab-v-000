4b0a591d2f3ace5e67f213b8353d44e57667e5f4
Denise Pendleton <dpendl1@hotmail.com>
2018-05-21 02:09:21 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
+class MyValidator < ActiveModel::Validator
+  def validate(post)
+    if  post.title == nil
+      post.errors[:title] << "Title can't be nil"
+    elsif post.title.starts_with? 'True'
+      post.errors[:title] << "Must be click bait"
+    else
+      post.errors[:title]
+    end
+  end
+end
+
+
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 150 }
+ validates :summary, length: { minimum: 5 }
+   validates :summary, length: { maximum: 150 }
+   validates_exclusion_of :category, :in => ["Bowling Ball"]
+   validates_with MyValidator
+
+
 end
