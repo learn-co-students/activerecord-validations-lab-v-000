8b6d768558e570ae695878ad6302648733bb5aab
Ally Kadel <akadel01@gmail.com>
2018-05-26 03:22:18 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    @record = record
+    flag = 0
+    bait = ["Won't Believe", "Secret", "Top", "Guess"]
+    for word in bait
+      if @record.title && @record.title.include?(word)
+        flag = 1
+      end
+    end
+      @record.errors[:title] << 'wrong stuff' if flag == 0
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { :in => %w(Fiction Non-Fiction) }
+  validates_with MyValidator
 end
