89dcc21a8a91246f3db357df480d9efe8d714366
Carol Greene <carolgreene3@gmail.com>
2018-05-27 15:04:33 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
+require 'pry'
 class Post < ActiveRecord::Base
-end
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid catagory"}
+
+
+    CLICKBAIT_PHRASES = [
+      "Won't Believe", 
+      "Secret",
+      "Top[number]",
+      "Guess"
+      ]
+
+      def is_clickbait?
+        if CLICKBAIT_PHRASES.none? { |phrase| phrase.match title }
+
+          errors.add(:title, "must be clickbait")
+        end
+      end
+    end
