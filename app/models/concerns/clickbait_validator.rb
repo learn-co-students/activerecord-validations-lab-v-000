3a17e6b89cd594e9c9be31b4c5495dd7f10bc5f2
Nicholas Powers <njpowers7915@gmail.com>
2018-06-28 22:39:24 +0000

    Done.

--- app/models/concerns/clickbait_validator.rb
+++ app/models/concerns/clickbait_validator.rb
@@ -1,11 +1,7 @@
 class ClickBaitValidator < ActiveModel::Validator
     def validate(record)
-        if record.title != nil
-            if record.title.downcase.include?("won't believe") || record.title.downcase.include?(/top\s\d+/.to_s) || record.title.downcase.include?("secret") || record.title.downcase.include?("guess")
-                return true
-            else
-                return false
-            end
+        if !record.title.downcase.include?("won't believe") && !record.title.downcase.include?(/top\s\d+/.to_s) && !record.title.downcase.include?("secret") && !record.title.downcase.include?("guess")
+            record.error.add(:title, "is not clickbait")
         end
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,5 @@
-class ClickBaitValidator < ActiveModel::Validator
-    def validate(record)
-        if record.title != nil
-            if record.title.downcase.include?("won't believe") || record.title.downcase.include?(/top\s\d+/.to_s) || record.title.downcase.include?("secret") || record.title.downcase.include?("guess")
-                return true
-            else
-                return false
-            end
-        end
-    end
-end
-
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
     validates :title, presence: true
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250}
