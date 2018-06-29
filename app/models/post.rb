161bab782a66c62e541704cc4d9afac39bb13847
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-29 04:12:34 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates :summary, length: { maximum: 50 }
+  validates :category, inclusion: { in: %w(Fiction) }
+  validate :clickbait
+
+  def clickbait
+    if title.include?(/(won't Believe|Secret|Top|Guess)/)
+      errors.add(:title, “cannot be clickbait-y”)
+    end
+  end
+
 end
