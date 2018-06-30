4a3965e1bf49ee9f6f60728ca7c9de3f1d6598af
ElvinGarcia <dimix181@gmail.com>
2018-06-29 22:19:15 -0400

    all test passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: { in:[ "Fiction" ,"Non-Fiction"] }
-    validate  :clickbait
+    validate  :clickbait, if: :title, presence: true
 
 
     CLICKABAITABLE = [/Won't Believe/, /Secret/,/Guess/]
