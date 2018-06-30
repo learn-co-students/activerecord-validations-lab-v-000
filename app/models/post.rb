728ed6f2828bedf67935daa045d46352f39fdd5b
ElvinGarcia <dimix181@gmail.com>
2018-06-29 22:11:44 -0400

    all test passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,17 @@ class Post < ActiveRecord::Base
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: { in:[ "Fiction" ,"Non-Fiction"] }
+    validate  :clickbait
+
+
+    CLICKABAITABLE = [/Won't Believe/, /Secret/,/Guess/]
+
+
+    def clickbait
+      unless self.title == nil
+      if CLICKABAITABLE.collect{ |reg| self.title.match(reg) }.compact.empty?
+        errors[:title] << "Must be clickbait"
+      end
+      end
+    end
 end
