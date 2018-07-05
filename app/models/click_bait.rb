4492ffee49e05a0e5339f4735c3b92199ca88f9e
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-05 21:45:45 +0000

    Done.

--- app/models/click_bait.rb
+++ app/models/click_bait.rb
@@ -1,8 +1,11 @@
 class ClickBait < ActiveModel::Validator
-  def validate(post)
-    unless
-    ["Won't Believe", "Secret", "Top", "Guess"].any? {|b| post.title.include? b}
-      post.errors[:title] << "Add "Won't believe", "Secret", "Top [number]" or "Guess" to make your title click-baity!"
+  def validate(record)
+    if
+
+    record.title.include?("Won't Believe") || record.title.include?("Secret") || record.title.include?("Top") || record.title.include?("Guess")
+    #["Won't Believe", "Secret", "Top", "Guess"].any? {|b| (record.title).include? b}
+      record.errors[:title] << "Edit the title to make it click-baity!"
     end
   end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,19 @@
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
-  validates_with ClickBait
+  validate :click_bait
+
+  private
+
+  def click_bait
+    if self.title != nil
+      unless
+        (self.title.include?("Won't Believe")) || (self.title.include?("Secret")) || (self.title.include?("Top")) || (self.title.include?("Guess"))
+        self.errors[:title] << "Edit the title to make it click-baity!"
+      end
+    end
+  end
+
 end
