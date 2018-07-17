a37bdde51ad4a7e9928d3e261dac92e3adf0b451
Jul Jen <juliajen.prog@gmail.com>
2018-07-17 07:57:13 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 150 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :is_clickbait?
+
+#add a custom validator to Post that ensures the title is sufficiently clickbait-y
+#If the title does not contain "Won't Believe", "Secret", "Top [number]", or "Guess", the validator should return false.
+  def is_clickbait?
+    bait = [/Won't Believe/i, /Secret/i, /Top[0-9]*/i, /Guess/i]
+    bait.each do |b|
+      if self.title.nil? || !self.title.match(b)
+        errors.add(:title, "not clickbait-y")
+      elsif self.title.match(b)
+        return true
+      end
+    end
+  end
+
 end
