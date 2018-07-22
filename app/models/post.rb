a2e524727bf6a4f6c87415be5e86b03ce3d72033
Chris Alberti <christopherjalberti@gmail.com>
2018-07-22 19:58:37 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validate :title_has_keywords
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-fiction)}
+
+  def title_has_keywords
+    if !title.nil?
+      unless ["Won't Believe", "Secret", "Top [number]", "Guess"].any? {|phrase| title.include?(phrase)}
+        errors[:title] << 'Need a catcy title please!'
+      end
+    end
+  end
 end
