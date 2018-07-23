1189b773658bfa43fdc04b96c2c4159423dd5efe
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-23 04:34:22 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,18 @@
+require 'pry'
 class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :content, { :length => { minimum: 250 } }
     validates :summary, { :length => { maximum: 250 } }
     validates :category, inclusion: ['Fiction', 'Non-Fiction']
+    validate :clickbait_y
+    def clickbait_y
+      if self.title
+        found = ["Won't Believe", "Secret", "Top ", "Guess"].any? { |click|
+              self.title.include?(click)
+        }
+        if !found
+          errors.add(:title,"Not clickbait-y")
+        end
+      end
+    end
 end
