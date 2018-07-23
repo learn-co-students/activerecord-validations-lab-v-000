f5c21d36ff21818002841e764ba340bdd274e21f
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-23 04:36:13 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,15 +4,28 @@ class Post < ActiveRecord::Base
     validates :content, { :length => { minimum: 250 } }
     validates :summary, { :length => { maximum: 250 } }
     validates :category, inclusion: ['Fiction', 'Non-Fiction']
-    validate :clickbait_y
-    def clickbait_y
-      if self.title
-        found = ["Won't Believe", "Secret", "Top ", "Guess"].any? { |click|
-              self.title.include?(click)
-        }
-        if !found
-          errors.add(:title,"Not clickbait-y")
-        end
-      end
+    validate :is_clickbait
+    # def is_clickbait
+    #   if self.title
+    #     found = ["Won't Believe", "Secret", "Top ", "Guess"].any? { |click|
+    #           self.title.include?(click)
+    #     }
+    #     if !found
+    #       errors.add(:title,"Not clickbait-y")
+    #     end
+    #   end
+    # end
+    # Better solution
+    CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def is_clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "Not clickbait-y")
     end
+  end
 end
