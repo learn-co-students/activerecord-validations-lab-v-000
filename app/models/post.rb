0a94b0454029fffc79e62257c10136bc2bee6d79
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-24 21:34:04 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,13 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  
+  validate :clickbait
+  
+  def clickbait
+    if !/Won't Believe|Secret|Top \d*|Guess/.match(title)
+    errors.add(:title, "is not clickbait-y enough")
+   end
+  end
+ 
 end
