9ade794fcf98b185dd013341c9c534dd0482f2c1
taryn yaeger <tsyaeger@gmail.com>
2018-07-26 20:20:24 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,11 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 } 
   validates :category, inclusion: { in: ["Fiction","Non-Fiction"] }
   validates :category, exclusion: { in: [nil] }
+  
+  def clickbait 
+    unless ["Won't Believe", "Secret", /[Top]+\s+\d*/, "Guess"]
+      erros.add(:title, "Must be clickbaity")
+    end
+  end
+  
 end
