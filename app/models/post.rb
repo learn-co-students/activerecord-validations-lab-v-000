9913f451dc71b009682c29f0f56dd198d5031479
epic0448 <samuelogundiran@Yahoo.com>
2018-08-01 12:28:58 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,14 @@ class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
-    validates :category, inclusion: { in: %w(fiction)}
+    validates :category, inclusion: { in: %w(Fiction)}
+    validate :is_clickbait? 
+   
+    def is_clickbait?
+        words =  [/Won't Believe/, /Secret/, /Top [0-9]/, /Guess/]
+         if words.none?{|word|word.match title}  
+         errors.add(:title, "word dont match")
+         end
+    end
 
-end
+end #end of class
