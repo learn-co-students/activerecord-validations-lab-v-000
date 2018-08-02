3b0477ee05c9835dd663b98442b8a967a89619dd
Elaine Anderson <eparie@bu.edu>
2018-08-02 20:13:17 +0000

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+
+  validate :title_must_be_clickbait
+
+  def title_must_be_clickbait
+#  binding.pry
+
+    if !title.to_s.include?("Won't Believe"|| "Secret"|| "Top [number]" || "Guess")
+      errors.add(:title, "must be click bait-y!")
+    end
+  end
+
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Nonfiction)}
+
 end
