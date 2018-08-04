c98c643fb2643124256e2b4d7ae94d2e85cd1e5f
Bryan Hooper <bryan@lindsayfarm.net>
2018-08-04 11:43:32 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: %w(Fiction Non-fiction)
+  validate :title_must_be_clickbaity
+
+  def title_must_be_clickbaity
+    if self.title != nil
+      result = !self.title.scan(/Won't Believe|Secret|Guess|Top \d/).empty?
+    else 
+      result = false
+    end
+    result
+  end
+  
 end
