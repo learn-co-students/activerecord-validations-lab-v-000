5ca0d696d5a32f5554c7e7fc2db090d5aee342a8
arye-dov-eidelman <aryedov.e@gmail.com>
2018-08-09 12:45:49 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  attr_reader :allowed_genres
+  validates :title, presence: true
+  validates :content, length: {minimum: 10}
+  validates :summary, length: {maximum: 299}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    if !/Won't Believe|Secret|Top \d{1,3}|Guess/i.match(title)
+      errors.add(:title, "must be clickbate")
+    end
+  end
 end
