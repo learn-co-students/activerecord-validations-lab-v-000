74a7560f545d1cb25f005aa0156e70c7814ef871
Kachel Wittig <kjwittig@gmail.com>
2018-08-14 17:03:09 -0500

    how needs peer validaiton when you can just make your own:

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
+require 'pry'
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :summary, length: { maximum: 10 }
+  validates :category, inclusion: { in: %w(Non-fiction Fiction) }
+  validates_with MyValidator
 end
