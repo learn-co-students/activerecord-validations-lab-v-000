b33032525d1d54c1395dbe682efcd5c62218a2a2
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-17 15:40:56 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,22 +9,22 @@ class Post < ActiveRecord::Base
   private
   
     def clickbait_title
-      words = ["Won't Believe", "Secret", "Guess"]
+      if title
+        words = ["Won't Believe", "Secret", "Guess"]
+        
+        has_words = words.any? do |word|
+          title.include?(word)
+        end
+  
+        unless has_words || title.match(/Top [0-9]/)
+          errors.add(:title, "Needs more clickbait!")
+        end
+        
+      end
       
-      has_words = words.any? do |word|
-        title.include?(word)
-      end
-
-      binding.pry
-
-      unless has_words || title.match(/Top [0-9]/)
-        errors.add(:title, "Needs more clickbait!")
-      end
     end
     
 end
 
-# title - required, must contain one of the following:  "Won't Believe", "Secret", "Top [number]", or "Guess"
-# https://guides.rubyonrails.org/active_record_validations.html#performing-custom-validations
-# must contain one of the following: "Won't Believe", "Secret", "Top [number]", or "Guess"
+
 
