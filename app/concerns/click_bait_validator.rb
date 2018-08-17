fbab309ced8eb19945f9d0409cb1970fb0fd7416
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-17 15:02:44 +0000

    Done.

--- app/concerns/click_bait_validator.rb
+++ app/concerns/click_bait_validator.rb
@@ -3,6 +3,10 @@ class ClickBait < ActiveModel::Validator
   
   def validate(title)
     # https://guides.rubyonrails.org/active_record_validations.html#performing-custom-validations
+    # must contain one of the following: "Won't Believe", "Secret", "Top [number]", or "Guess"
+    unless title.include?
+      record.errors[
+    end
   end
   
 end
