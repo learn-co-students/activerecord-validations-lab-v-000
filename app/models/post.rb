d68cab5863aee8c6042ea77fafc3f862d96657fc
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-17 15:28:24 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,26 +1,24 @@
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations 
-  validates_with ClickBait
   
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validates :content, length: { minimum: 250 }
   validates :title, presence: true 
-  # title - required, must contain one of the following:  "Won't Believe", "Secret", "Top [number]", or "Guess"
+  validate :clickbait_title
   
+  private
+  
+    def clickbait_title
+      words = ["Won't Believe", "Secret", "Guess"]
+      
+      unless title.in?(words) || title.match?(/Top [0-9]/)
+        errors.add(:title, "Needs more clickbait!")
+      end
+    end
+    
 end
 
+# title - required, must contain one of the following:  "Won't Believe", "Secret", "Top [number]", or "Guess"
 # https://guides.rubyonrails.org/active_record_validations.html#performing-custom-validations
 # must contain one of the following: "Won't Believe", "Secret", "Top [number]", or "Guess"
 
-class ClickBait < ActiveModel::Validator
-  
-  def validate(title)
-    words = ["Won't Believe", "Secret", "Guess"]
-    
-    unless title.in?(words) || title.match?(/Top [0-9]/)
-      record.errors[title] << "Needs more clickbait!"
-    end
-  end
-  
-end
