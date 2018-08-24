9fdb121002461de1aaea77f4a3b9f6befa50ed10
Jamie Brown <jmbrowncow@gmail.com>
2018-08-24 19:11:49 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,17 @@ class Post < ActiveRecord::Base
       errors.add(:title, "needs to be clickbait-y")
     end
   end
+
+  #   CLICKBAIT_PATTERNS = [
+  #   /Won't Believe/i,
+  #   /Secret/i,
+  #   /Top [0-9]*/i,
+  #   /Guess/i
+  #   ]
+  #
+  # def is_clickbait?
+  #   if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+  #     errors.add(:title, "must be clickbait")
+  #   end
+  # end
 end
