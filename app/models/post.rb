2df3231247178bf88f4923f711c9751b9f662888
Pan Cheng <pchengeducation@hotmail.com>
2018-08-28 23:53:44 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates :summary, length: { maximum: 100 }
+  validates :category, inclusion: { in: %w(Fiction Non-fiction) }
+  validate :is_clickbait
+
+  def is_clickbait
+    clickbait = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i ]
+    clickbait.each do |phrase|
+      if phrase.match(title)
+        return
+      end
+    end
+    errors.add(:title, "is clickbait")
+  end
 end
