69694373e840154b8a2b12d6f99a7074b3996913
Christine Cua <cloudycua@gmail.com>
2018-09-10 18:13:23 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,14 @@
 class Post < ActiveRecord::Base
+# All posts have a title
   validates :title, presence: true
+# Post content is at least 250 characters long
   validates :content, length: { minimum: 250 }
+# Post summary is a maximum of 250 characters
   validates :summary, length: { maximum: 250 }
+# Post category is either Fiction or Non-Fiction
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+# Ensures the title is sufficiently clickbait-y. If does't contain
+# catchy phrase, validator will return false
   validate :is_clickbait?
 
   CLICKBAIT_PATTERNS = [
