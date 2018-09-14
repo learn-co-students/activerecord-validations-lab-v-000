9dc43640413d9c0078a12c12ee318e220e600f51
asabogal <asabogal.git@gmail.com>
2018-09-14 19:14:17 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbaity
+
+  def clickbaity
+    if self.title && !(title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top #{/^[0-9]*$/}"))
+      errors.add(:title, "needs to be clickbait-y")
+    end
+  end
+
 end
