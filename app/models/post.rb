49c6b4d806a0b9e93e28eab6b00bbdf5a44f18f2
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-18 01:58:24 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,15 @@
 class Post < ActiveRecord::Base
+
   validates :title, presence: true
-  validates(:content, { :length => { :is => 250 } })
+  validates :content, length: { minimum: 250 }
   validates :category, inclusion: { in: %w( Fiction Non-Fiction)}
   validates :summary, { :length => { :maximum => 250 } }
+  validate :message_validator #custom
 
-  validates :title, inclusion: { in: %w("Won't Believe" Secret Guess "Top [number]" )}, message: false
+  @@words = [/Won't Believe/, /Secret/, /Top [0-9]*/, /Guess/]
+ def message_validator
+    if @@words.none?{|word| word.match title} #match if true returns the value  and if false = nil
+        errors.add(:title, "Must change title")
+    end
+ end
 end
