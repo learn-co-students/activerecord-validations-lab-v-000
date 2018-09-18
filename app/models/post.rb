b12c59c85f854e9774ef0344bc611dd8ca923b56
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-18 00:06:14 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-#  validates(:content, { :length => { :is => 250 } })
-#  validates(:summary, { :length => { :is =< 250 } })
+  validates(:content, { :length => { :is => 250 } })
+  validates :category, inclusion: { in: %w( Fiction Non-Fiction)}
+  validates :summary, { :length => { :maximum => 250 } }
+
+  validates :title, inclusion: { in: %w("Won't Believe" Secret Guess "Top [number]" )}, message: false
 end
