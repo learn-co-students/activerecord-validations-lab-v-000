1286d4061cb641e8d2f53ab0a96e1cf0b8a8714e
Anu Khambete <anukhambete@gmail.com>
2018-09-25 11:04:40 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :post_is_clickbait
+
+  def post_is_clickbait
+    if !["Won't Believe", "Secret", "Top [number]", "Guess"].include?(title)
+      errors.add(:title, "change name")
+    end
+  end
+
+
 end
