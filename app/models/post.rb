5b261c1b89121693c2594ed872076b5c9b49264d
Anu Khambete <anukhambete@gmail.com>
2018-09-25 12:17:38 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
@@ -6,9 +7,18 @@ class Post < ActiveRecord::Base
   validate :post_is_clickbait
 
   def post_is_clickbait
-    if !["Won't Believe", "Secret", "Top [number]", "Guess"].include?(title)
-      errors.add(:title, "change name")
-    end
+    #if !["Won't Believe", "Secret", "Top [number]", "Guess"].include?(title)
+
+    words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    temp = nil
+    #binding.pry
+      if title.present? && words.any? {|word| title.include?(word)}
+        #binding.pry
+      temp = true
+      else
+      errors.add(:title, "not clickbait-y")
+      end
+
   end
 
 
