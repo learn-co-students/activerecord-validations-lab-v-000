1be059bfe7411c4c6eef42251083863d71bfc967
Katrina Pritchard <pritckat@gmail.com>
2018-09-25 16:42:26 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  # include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: {minimum: 250 }
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  # validates_with MyValidator
 end
+#
+# class MyValidator < ActiveModel::Validator
+#   def validate(post)
+#     unless post.title.starts_with?("Won't Believe", "Secret", "Top", "Guess")
+#       record.errors[:title] << 'Need a clickbait-y title please!'
+#     end
+#   end
+# end
