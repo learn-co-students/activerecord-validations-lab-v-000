41339daeccb1c0fcb7645b6d7a49fbe0fe73703a
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-28 01:21:18 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction )}
+  validate :clickbaity
+
+  def clickbaity
+
+    if !title.nil? && (!title.include?("Won't Believe" || "Secret" || "Top" ||"Guess"))
+      errors.add(:title, "Not clickbaity enough")
+    end
+  end
 end
