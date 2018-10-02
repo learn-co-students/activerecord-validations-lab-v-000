b5c55b5bd607e7d362c9f7f3b988d139ab956d22
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-10-02 10:46:41 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
+class ClickBaitValidator < ActiveModel::Validator
+  def validate(record)
+    if record.title == nil
+      false
+    elsif !record.title.include?("Won't Believe")
+      record.errors[:title] << 'Please make your title more clickbait-y'
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: :true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates_with ClickBaitValidator
 end
