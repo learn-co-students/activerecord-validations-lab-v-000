b7f67875a45e5b491a37d85a2999cc93e4e31b1d
github username <github email address>
2018-10-03 14:26:20 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+   validate(:title)
+
+
+
+
+  def validate(record)
+    validates :record, inclusion: { in: ["Won't Believe", "Secret", "Top [number]", "Guess"]}
+  end
+  #binding.pry
 end
