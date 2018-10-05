1a8a0c3b8081b1d466b9565617d0497a3329c5f0
micahshute <micah.shute@gmail.com>
2018-10-05 09:49:52 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+    
+    validates :title, presence: true
+    validate :is_clickbait
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    
+
+    def is_clickbait
+        included = false
+        @@phrases.each { |phrase| included = true if self.title && self.title.downcase =~ phrase }
+        if not included
+            errors.add(:title, "You need a more exciting title")
+        end
+    end
+
+    @@phrases = [/won't believe/, /secret/, /top \d/, /guess/]
+
+
 end
