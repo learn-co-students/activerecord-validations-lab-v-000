e963638eb33a0ca148d21d66ae757ad4363d246e
btmccollum <btmcco8@gmail.com>
2018-10-31 15:37:54 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 } 
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :click_bait
+
+    def click_bait
+        click_bait = [/Won't Believe/i, /Secret/i, /Top [\d*]/i, /Guess/i]
+
+        if click_bait.none? { |words| words.match title}
+            errors.add(:title, "does not have enough clickbait")
+        end
+    end
 end
