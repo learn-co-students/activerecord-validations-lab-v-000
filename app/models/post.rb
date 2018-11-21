af9b0cc90dc0d93317b0bab966371d242d239886
amitnyc83 <amit_nyc@live.com>
2018-11-21 08:16:40 -0500

    passing test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
-end
+  category_name = ['Fiction', 'Non-Fiction']
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, presence: true, inclusion: category_name
+  validate :clickbait?
+
+  CLICKBAIT_PATTERNS = [
+		/Won't Believe/i,
+		/Secret/i,
+		/Top [0-9]*/i,
+		/Guess/i
+	]
+
+  
+ 	def clickbait?
+		if CLICKBAIT_PATTERNS.none? { |p| p.match title }
+			errors.add(:title, "must be clickbait")
+		end
+	end
+  end
