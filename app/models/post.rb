4a940b332cbc38afc0b60b3ab265dca8651bfb5e
lauradarlak <lauradarlak@gmail.com>
2018-11-29 18:07:25 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,5 +7,21 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Nonfiction)}
   validates_with NonClickbait
 
+  # alternate code
+  # validate :is_clickbait?
+  #
+  # CLICKBAIT_PATTERNS = [
+  #   /Won't Believe/i,
+  #   /Secret/i,
+  #   /Top [0-9]*/i,
+  #   /Guess/i
+  # ]
+  #
+  # def is_clickbait?
+  #   if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+  #     errors.add(:title, "must be clickbait")
+  #   end
+  # end
+
 
 end
