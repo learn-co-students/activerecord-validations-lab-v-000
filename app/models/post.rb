74eb1fa29a9de6f14716e02ad4a281e6e3a1ee44
Judy McGoldrick <jjeanius@verizon.net>
2019-01-08 05:41:56 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  validator :Post, validates_with:
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 250}
+  validates :summary, presence: true, length: { maximum: 250}
+  validates :category, inclusion: {in: %w(Ficture  Non-Fiction)}
+
 end
+# Post category is either Fiction or Non-Fiction This step requires
+# an inclusion validator, which was not outlined in the README lesson. You'll need to
+# refer to the Rails guide to look up how to use it.
