bb6b908853464dcf30293aeeb0389df98559ceca
Harley Rayner <harleymteden@gmail.com>
2019-01-17 21:52:33 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :summary , length: { maximum: 100 }
+  # validates :category, presence: true
+
+  validates :category, inclusion: { in: Post.each {|post| post.category }
+
 end
