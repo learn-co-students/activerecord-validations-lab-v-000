6f3cc0207dd9072aca8bcde471b0c4b8d973ba40
Harley Rayner <harleymteden@gmail.com>
2019-01-18 17:53:48 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'pry'
 gem 'rails'
 gem 'sqlite3'
 gem 'sass-rails'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,22 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true
-  validates :content, length: { minimum: 10 }
-  validates :summary , length: { maximum: 100 }
-  # validates :category, presence: true
 
-  validates :category, inclusion: { in: Post.each {|post| post.category }
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary , length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :title_must_be_clickbaity
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def title_must_be_clickbaity
+    if CLICKBAIT_PATTERNS.none?  { |pattern| pattern.match(self.title) } 
+      errors.add(:title, "False")
+    end
+  end
 
 end
