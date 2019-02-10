ab25bd5db31914588176aa64e1d274a5611b7db6
Erin Green <erin.y.green@gmail.com>
2019-02-10 18:31:15 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, inclusion: {in: (fiction non-fiction)}
+  validates :category, inclusion: {in: ('fiction' 'non-fiction')}
 
   def validate(post)
     unless post.title.includes? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
