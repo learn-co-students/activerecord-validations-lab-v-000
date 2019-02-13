1c29d4c7abcf865c039ca890a273c64d2c7f0ee6
Brad Schrag <brad.schrag@gmail.com>
2019-02-13 09:14:57 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: { minimum: 250 }
+	validates :summary, length: { maximum: 250 }
+	validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+	validate :click_bait
+
+	def click_bait
+		if title.nil?
+			false
+		elsif !title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top ") || title.include?("Guess")
+			errors.add(:title, "isn't click bait")
+		end
+	end
 end
+
+
