07f02e8781c0e84cc78ac89dde2a753b7936adaa
Carley Tripp <carley.tripp@outlook.com>
2019-02-14 22:41:06 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait_title
+
+  def clickbait_title
+
+    if title !~ /Won't\sBelieve|Secret|Top\s\d|Guess/i
+      errors.add(:title, 'must include clickbait')
+    end
+    
+  end
+
 end
