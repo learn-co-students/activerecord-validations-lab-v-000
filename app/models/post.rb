18d427caff478bd749dee3344d295f43c3bb6bb2
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-25 01:34:44 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :validate_click
+
+
+  def validate_click
+    if !title.nil? &&
+      !title.include?("Won't Believe") &&
+      !title.include?("Secret") &&
+      !title.include?("Guess") &&
+      !title.include?("Top")
+         errors.add(:title, "not clickbait-y")
+      end
+    end
+
 end
