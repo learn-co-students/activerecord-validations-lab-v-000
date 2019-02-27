2366bc3c46610893ed48f96aff422af7a8424b97
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-02-27 05:35:10 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+  #all authors have a name
+  validates :name, presence: true
+  #No two authors have the same name
+  validates :name, uniqueness: true
+  #Author phone number are 10 digits
+  validates :phone_number, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  #All posts have a title
+  validates :title, presence: true
+  #Post content is at least 250 characters
+  validates :content, length: { minimum: 250}
+  #post summary is maximum of 250 characters
+  validates :summary, length: { maximum: 250}
+  #Post category is either fiction or non-fiction
+  validates :category, inclusion: { in: %w(Fiction Non-fiction)}
+  validate :clickbait?
+
+  def clickbait?
+    if self.title && !self.title.match(/Won't Believe|Secret|Top [\d*]|Guess/)
+    errors.add(:clickbait, "is not enticing enough")
+  end
+end
 end
