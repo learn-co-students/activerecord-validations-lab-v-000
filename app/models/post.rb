877efc604474ae878ce6e0a79ef88b70ee1cd296
Heather Miller <heather.miller114@gmail.com>
2019-03-17 18:56:13 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-    #validates :title, presence: true
+    validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
@@ -9,8 +9,13 @@ class Post < ActiveRecord::Base
     private
 
     def post_title_includes
-        unless title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+        if title.valid? == 'true'
+            unless title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+                errors.add(:title, "false")
+            end
+        else
             errors.add(:title, "false")
+        end
         end
     end
 end
