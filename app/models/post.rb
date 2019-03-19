9a3bade15e060c31f886344bcc726441416e75bc
github username <github email address>
2019-03-18 22:29:19 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,18 @@
+require 'pry'
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbait
 
-  def validate(post)
-    unless post.title.include?("Won't Believe") || post.title.include?("Secret") || post.title.include?("Top [number]") || post.title.include?("Guess")
+  
+
+  def clickbait
+    if self.title
+      if !self.title.include?("Won't Believe") && !self.title.include?("Secret") && !self.title.include?("Top [number]") && !self.title.include?("Guess")
+        errors.add(:title, "Not clickbait!!!!")
+      end
+    end
   end
 end
