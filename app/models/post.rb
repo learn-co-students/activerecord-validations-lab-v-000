00a350abeeec19e82a911cb49c460f6a78563d4a
Ashley Wong <ashwong04@gmail.com>
2019-04-04 14:26:26 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :clickbait, presence: true
+
+  def clickbait
+    bait = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+    if title
+      bait.each {|word| title.include?(word)}
+    end
+  end
+
 end
