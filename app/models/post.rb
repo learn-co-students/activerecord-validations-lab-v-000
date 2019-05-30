2490846e9db4051eda021ba0776dc92fb0c2d226
MisterSenor <nathan.visak@gmail.com>
2019-05-30 09:55:00 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+# class MyValidator < ActiveModel::Validator
+#   def validate(post)
+#     unless !post.title.include?("won't believe") || !post.title.include?("top [number]") || !post.title.include?("secret") || !post.title.include?("guess")
+#       post.errors[:title] << 'Needs a more click-baity title!'
+#     end
+#   end
+# end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with MyValidator
+  validates :title, presence: true
+  validates :content, presence: true, length: {minimum: 250}
+  validates :summary, presence: true, length: {maximum: 250}
+  validates :category, presence: true, inclusion: { in: %w(Fiction Non-Fiction)}
 end
