9e9c3e3ba2109b8b466e920b2b978c822e856058
github username <github email address>
2019-06-28 13:02:37 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validates :title, presence: true, format: {with: /Won't Believe|Secret|Guess|Top [\d]*/i}
+    #used rubular to handle the regex
+    #https://stackoverflow.com/questions/40545638/rails-validate-format-with-regex
 end
