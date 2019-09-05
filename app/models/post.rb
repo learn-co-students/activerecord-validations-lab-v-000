8d7eb3ac6a859e44ea0ab6f249a43cd224e581cc
David Forer <david.j.forer@gmail.com>
2019-09-05 12:43:42 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :summary, length: { in: 6..20 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
+  validate :clickbait
+
+  private
+  def clickbait
+    clickbait_arr = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+    if clickbait_arr.none? do |i|
+      i.match(title)
+    end
+      self.errors[:title] << "You need to be more clickbait-y"
+    end
+  end
 end
