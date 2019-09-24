c9783965892528c864c52ff920d3f4c5cd4757d2
Scott Silverman <scottms724@gmail.com>
2019-09-24 14:19:42 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
-end
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+  message: "%{value} is not a valid category" }
+  validates_with Validator
+end 
