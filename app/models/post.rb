9ebabde655bf694fbfea86be2dcb617747e9a0a3
essbee808 <sheena.myb@gmail.com>
2020-11-13 14:27:27 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,20 @@ class Post < ActiveRecord::Base
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 } 
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_include
+
+    CLICKBAIT_PATTERNS = [
+        "Won't Believe",
+        "Secret",
+        "Top [number]",
+        "Guess"
+    ]
+    #custom validators
+    def title_include
+    #if the title does not contain "Won't Believe", "Secret", "Top [number]", or "Guess"
+    #the validator should add a validation
+        if title.present? && CLICKBAIT_PATTERNS.none? { |p| title.match(p) }
+            errors.add(:title, "title not clickbait-y enough")
+        end
+    end
 end
